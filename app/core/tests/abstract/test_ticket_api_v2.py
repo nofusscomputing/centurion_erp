9a35d023d9c568c76bd7f6434088ebf1d000a91f
Jon <jonathon.lockwood@networkedweb.com>
2025-01-24 17:21:58 +0930

    test(core): Unit test cases for ticket impact_badge field checks
    ref: #491 closes #488

--- app/core/tests/abstract/test_ticket_api_v2.py
+++ app/core/tests/abstract/test_ticket_api_v2.py
@@ -242,6 +242,117 @@ class TicketAPI(
 
 
 
+    def test_api_field_exists_impact_badge(self):
+        """ Test for existance of API Field
+
+        impact_badge field must exist
+        """
+
+        assert 'impact_badge' in self.api_data
+
+
+    def test_api_field_type_impact_badge(self):
+        """ Test for type for API Field
+
+        impact_badge field must be int
+        """
+
+        assert type(self.api_data['impact_badge']) is dict
+
+
+    def test_api_field_exists_impact_badge_icon(self):
+        """ Test for existance of API Field
+
+        impact_badge.icon field must exist
+        """
+
+        assert 'icon' in self.api_data['impact_badge']
+
+
+    def test_api_field_type_impact_badge_icon(self):
+        """ Test for type for API Field
+
+        impact_badge.icon field must be dict
+        """
+
+        assert type(self.api_data['impact_badge']['icon']) is dict
+
+
+    def test_api_field_exists_impact_badge_icon_name(self):
+        """ Test for existance of API Field
+
+        impact_badge.icon.name field must exist
+        """
+
+        assert 'name' in self.api_data['impact_badge']['icon']
+
+
+    def test_api_field_type_impact_badge_icon_name(self):
+        """ Test for type for API Field
+
+        impact_badge.icon.name field must be str
+        """
+
+        assert type(self.api_data['impact_badge']['icon']['name']) is str
+
+
+    def test_api_field_exists_impact_badge_icon_style(self):
+        """ Test for existance of API Field
+
+        impact_badge.icon.style field must exist
+        """
+
+        assert 'style' in self.api_data['impact_badge']['icon']
+
+
+    def test_api_field_type_impact_badge_icon_style(self):
+        """ Test for type for API Field
+
+        impact_badge.icon.style field must be str
+        """
+
+        assert type(self.api_data['impact_badge']['icon']['style']) is str
+
+
+
+    def test_api_field_exists_impact_badge_text(self):
+        """ Test for existance of API Field
+
+        impact_badge.text field must exist
+        """
+
+        assert 'text' in self.api_data['impact_badge']
+
+
+    def test_api_field_type_impact_badge_text(self):
+        """ Test for type for API Field
+
+        impact_badge.text field must be str
+        """
+
+        assert type(self.api_data['impact_badge']['text']) is str
+
+
+
+    def test_api_field_exists_impact_badge_text_style(self):
+        """ Test for existance of API Field
+
+        impact_badge.text_style field must exist
+        """
+
+        assert 'text_style' in self.api_data['impact_badge']
+
+
+    def test_api_field_type_impact_badge_text_style(self):
+        """ Test for type for API Field
+
+        impact_badge.text_style field must be str
+        """
+
+        assert type(self.api_data['impact_badge']['text_style']) is str
+
+
+
     def test_api_field_exists_title(self):
         """ Test for existance of API Field
 
