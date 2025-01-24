40a15ed4f092e8b366f8424c2d9133dd44b40b33
Jon <jonathon.lockwood@networkedweb.com>
2025-01-24 17:23:54 +0930

    test(core): Unit test cases for ticket urgency_badge field checks
    ref: #491 closes #487

--- app/core/tests/abstract/test_ticket_api_v2.py
+++ app/core/tests/abstract/test_ticket_api_v2.py
@@ -464,6 +464,117 @@ class TicketAPI(
 
 
 
+    def test_api_field_exists_urgency_badge(self):
+        """ Test for existance of API Field
+
+        urgency_badge field must exist
+        """
+
+        assert 'urgency_badge' in self.api_data
+
+
+    def test_api_field_type_urgency_badge(self):
+        """ Test for type for API Field
+
+        urgency_badge field must be int
+        """
+
+        assert type(self.api_data['urgency_badge']) is dict
+
+
+    def test_api_field_exists_urgency_badge_icon(self):
+        """ Test for existance of API Field
+
+        urgency_badge.icon field must exist
+        """
+
+        assert 'icon' in self.api_data['urgency_badge']
+
+
+    def test_api_field_type_urgency_badge_icon(self):
+        """ Test for type for API Field
+
+        urgency_badge.icon field must be dict
+        """
+
+        assert type(self.api_data['urgency_badge']['icon']) is dict
+
+
+    def test_api_field_exists_urgency_badge_icon_name(self):
+        """ Test for existance of API Field
+
+        urgency_badge.icon.name field must exist
+        """
+
+        assert 'name' in self.api_data['urgency_badge']['icon']
+
+
+    def test_api_field_type_urgency_badge_icon_name(self):
+        """ Test for type for API Field
+
+        urgency_badge.icon.name field must be str
+        """
+
+        assert type(self.api_data['urgency_badge']['icon']['name']) is str
+
+
+    def test_api_field_exists_urgency_badge_icon_style(self):
+        """ Test for existance of API Field
+
+        urgency_badge.icon.style field must exist
+        """
+
+        assert 'style' in self.api_data['urgency_badge']['icon']
+
+
+    def test_api_field_type_urgency_badge_icon_style(self):
+        """ Test for type for API Field
+
+        urgency_badge.icon.style field must be str
+        """
+
+        assert type(self.api_data['urgency_badge']['icon']['style']) is str
+
+
+
+    def test_api_field_exists_urgency_badge_text(self):
+        """ Test for existance of API Field
+
+        urgency_badge.text field must exist
+        """
+
+        assert 'text' in self.api_data['urgency_badge']
+
+
+    def test_api_field_type_urgency_badge_text(self):
+        """ Test for type for API Field
+
+        urgency_badge.text field must be str
+        """
+
+        assert type(self.api_data['urgency_badge']['text']) is str
+
+
+
+    def test_api_field_exists_urgency_badge_text_style(self):
+        """ Test for existance of API Field
+
+        urgency_badge.text_style field must exist
+        """
+
+        assert 'text_style' in self.api_data['urgency_badge']
+
+
+    def test_api_field_type_urgency_badge_text_style(self):
+        """ Test for type for API Field
+
+        urgency_badge.text_style field must be str
+        """
+
+        assert type(self.api_data['urgency_badge']['text_style']) is str
+
+
+
     def test_api_field_exists_title(self):
         """ Test for existance of API Field
 
