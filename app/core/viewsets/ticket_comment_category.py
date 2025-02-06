02595ca010b467a844a068afcd485341a4347df6
Jon <jonathon.lockwood@networkedweb.com>
2025-02-06 17:36:48 +0930

    fix(core): Add missing attribute `view_description` to ticket comment category ViewSet
    ref: #518

--- app/core/viewsets/ticket_comment_category.py
+++ app/core/viewsets/ticket_comment_category.py
@@ -66,6 +66,8 @@ class ViewSet(ModelViewSet):
 
     model = TicketCommentCategory
 
+    view_description: str = 'Categories available for Ticket Comments'
+
 
     def get_serializer_class(self):
 
