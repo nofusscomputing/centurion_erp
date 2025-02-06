1e71d0d39b6d7dc4ae0cdc8e606c4d4170b1cee3
Jon <jonathon.lockwood@networkedweb.com>
2025-02-06 19:16:06 +0930

    fix(core): correct truthy check for celery log ViewSet property when evaluating queryset
    ref: #513 #518

--- app/core/viewsets/celery_log.py
+++ app/core/viewsets/celery_log.py
@@ -91,12 +91,19 @@ class ViewSet(AuthUserReadOnlyModelViewSet):
 
     def get_serializer_class(self):
 
+        if self.serializer_class is not None:
+
+            return  self.serializer_class
+
         if (
             self.action == 'list'
             or self.action == 'retrieve'
         ):
 
-            return globals()['TaskResultViewSerializer']
+            self.serializer_class = globals()['TaskResultViewSerializer']
 
+        else:
 
-        return globals()['TaskResultModelSerializer']
+            self.serializer_class = globals()['TaskResultModelSerializer']
+
+        return self.serializer_class
