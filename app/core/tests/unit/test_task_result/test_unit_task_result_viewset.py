f86038c16988fb7d686cc018083aa0768bbc7694
Jon <jonathon.lockwood@networkedweb.com>
2025-02-06 20:36:56 +0930

    test(core): add permisssion class override test case for celery results
    ref: #518

--- app/core/tests/unit/test_task_result/test_unit_task_result_viewset.py
+++ app/core/tests/unit/test_task_result/test_unit_task_result_viewset.py
@@ -3,6 +3,7 @@ import pytest
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
 
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.reverse import reverse
 
 from access.models import Organization
@@ -36,6 +37,23 @@ class ViewsetCommon(
         self.view_user = User.objects.create_user(username="test_view_user", password="password", is_superuser=True)
 
         self.kwargs = {}
+
+
+
+    def test_view_attr_permission_classes_value(self):
+        """Attribute Test
+
+        This test case is a duplicate to override the test case with the same
+        name. This class uses a different permission class.
+
+        Attribute `permission_classes` must be metadata class `ReactUIMetadata`
+        """
+
+        view_set = self.viewset()
+
+        assert view_set.permission_classes[0] is IsAuthenticated
+
+        assert len(view_set.permission_classes) == 1
 
 
 
