d03afa3017702852ac605235065fd4b87c652c91
Jon <jonathon.lockwood@networkedweb.com>
2025-02-07 21:32:06 +0930

    feat(itam): Device Model Notes ViewSet
    ref: #523 #526

--- app/api/urls_v2.py
+++ app/api/urls_v2.py
@@ -57,6 +57,7 @@ from itam.viewsets import (
     index as itam_index_v2,
     device as device_v2,
     device_model as device_model_v2,
+    device_model_notes,
     device_notes,
     device_type as device_type_v2,
     device_type_notes,
@@ -191,6 +192,7 @@ router.register('settings/celery_log', celery_log_v2.ViewSet, basename='_api_v2_
 router.register('settings/cluster_type', cluster_type_v2.ViewSet, basename='_api_v2_cluster_type')
 router.register('settings/cluster_type/(?P<model_id>[0-9]+)/notes', cluster_type_notes.ViewSet, basename='_api_v2_cluster_type_note')
 router.register('settings/device_model', device_model_v2.ViewSet, basename='_api_v2_device_model')
+router.register('settings/device_model/(?P<model_id>[0-9]+)/notes', device_model_notes.ViewSet, basename='_api_v2_device_model_note')
 router.register('settings/device_type', device_type_v2.ViewSet, basename='_api_v2_device_type')
 router.register('settings/device_type/(?P<model_id>[0-9]+)/notes', device_type_notes.ViewSet, basename='_api_v2_device_type_note')
 router.register('settings/external_link', external_link_v2.ViewSet, basename='_api_v2_external_link')
