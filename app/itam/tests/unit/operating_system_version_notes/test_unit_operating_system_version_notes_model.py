f26b0706ae30ffa0c57a87dcef358f9820c623b9
Jon <jonathon.lockwood@networkedweb.com>
2025-02-09 19:30:31 +0930

    test(itam): Operating System Version Note Model Checks
    ref: #525 closes #539

---
+++ app/itam/tests/unit/operating_system_version_notes/test_unit_operating_system_version_notes_model.py
@@ -0,0 +1,42 @@
+from django.contrib.contenttypes.models import ContentType
+from django.test import TestCase
+
+from core.tests.abstract.test_unit_model_notes_model import ModelNotesModel
+
+from itam.models.operating_system import OperatingSystem
+from itam.models.operating_system_version_notes import OperatingSystemVersionNotes
+
+
+
+class OperatingSystemVersionNotesModel(
+    ModelNotesModel,
+    TestCase,
+):
+
+    model = OperatingSystemVersionNotes
+
+
+    @classmethod
+    def setUpTestData(self):
+        """Setup Test"""
+
+        super().setUpTestData()
+
+
+        self.item = self.model.objects.create(
+            organization = self.organization,
+            content = 'a random comment for an exiting item',
+            content_type = ContentType.objects.get(
+                app_label = str(self.model._meta.app_label).lower(),
+                model = str(self.model.model.field.related_model.__name__).replace(' ', '').lower(),
+            ),
+            model = self.model.model.field.related_model.objects.create(
+                organization = self.organization,
+                name = 'note model existing item',
+                operating_system = OperatingSystem.objects.create(
+                    organization = self.organization,
+                    name = 'os'
+                ),
+            ),
+            created_by = self.user,
+        )
