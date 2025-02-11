f7818fe2e8868f615ba7706583cc9e0295b9cde5
Jon <jonathon.lockwood@networkedweb.com>
2025-02-11 20:24:55 +0930

    feat(api): Ticket Viewset requires its own function to obtain the model view serializer
    ref: #578 #477

--- app/core/viewsets/ticket.py
+++ app/core/viewsets/ticket.py
@@ -353,3 +353,24 @@ class TicketViewSet(ModelViewSet):
             self.serializer_class = globals()[serializer_prefix + 'TicketModelSerializer']
 
         return self.serializer_class
+
+
+    def get_view_serializer_name(self) -> str:
+        """Get the Models `View` Serializer name.
+
+        Override this function if required and/or the serializer names deviate from default.
+
+        Returns:
+            str: Models View Serializer Class name
+        """
+
+        if self.view_serializer_name is None:
+
+            self.view_serializer_name = super().get_view_serializer_name()
+
+            for remove_str in [ 'ChangeTicketView', 'AddTicketView', 'ImportTicketView', 'TriageTicketView' ]:
+
+                self.view_serializer_name = self.view_serializer_name.replace(remove_str, 'TicketView')
+
+
+        return self.view_serializer_name
