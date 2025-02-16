d273c35baa52cd7ee49c33ea001beb04a023adc8
Jon <jonathon.lockwood@networkedweb.com>
2025-02-16 23:29:28 +0930

    feat(itam): History Model migrations for Software
    ref: #605 #623

--- app/itam/viewsets/software.py
+++ app/itam/viewsets/software.py
@@ -1,5 +1,7 @@
 from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiResponse
 
+# THis import only exists so that the migrations can be created
+from itam.models.software_history import SoftwareHistory    # pylint: disable=W0611:unused-import
 from itam.serializers.software import (
     Software,
     SoftwareModelSerializer,
