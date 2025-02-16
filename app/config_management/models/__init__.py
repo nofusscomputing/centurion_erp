589c7fab26c0be0ee1b7d261083867d5271a4c5a
Jon <jonathon.lockwood@networkedweb.com>
2025-02-16 10:43:26 +0930

    feat(config_management): History Model for Config Group Hosts added
    ref: #605 #637

--- app/config_management/models/__init__.py
+++ app/config_management/models/__init__.py
@@ -1,1 +1,2 @@
 from .config_groups_history import ConfigGroupsHistory    # pylint: disable=W0611:unused-import
+from .config_groups_hosts_history import ConfigGroupHostsHistory    # pylint: disable=W0611:unused-import
