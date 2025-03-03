8a787a516fe765734ea8d5b1be80f86d9f4612d0
Jon <jonathon.lockwood@networkedweb.com>
2025-03-04 03:22:04 +0930

    fix(core): Add ability to add notes for ticket category
    ref: #662 #667

---
+++ app/core/serializers/ticket_category_notes.py
@@ -0,0 +1,42 @@
+from core.models.ticket.ticket_category_notes import TicketCategoryNotes
+
+from core.serializers.model_notes import (
+    ModelNotes,
+    ModelNoteBaseSerializer,
+    ModelNoteModelSerializer,
+    ModelNoteViewSerializer
+)
+
+
+
+class TicketCategoryNoteBaseSerializer(ModelNoteBaseSerializer):
+
+    pass
+
+
+class TicketCategoryNoteModelSerializer(
+    ModelNoteModelSerializer
+):
+
+
+    class Meta:
+
+        model = TicketCategoryNotes
+
+        fields =  ModelNoteModelSerializer.Meta.fields + [
+            'model',
+        ]
+
+        read_only_fields = ModelNoteModelSerializer.Meta.read_only_fields + [
+            'model',
+            'content_type',
+        ]
+
+
+
+class TicketCategoryNoteViewSerializer(
+    ModelNoteViewSerializer,
+    TicketCategoryNoteModelSerializer,
+):
+
+    pass
