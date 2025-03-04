492bbfb5210b82e0edc3dcf2dcb59e490ee0d310
Jon <jonathon.lockwood@networkedweb.com>
2025-03-05 02:29:40 +0930

    test(devops): software_feature_flag_enable viewset checks
    ref: #670 #664

---
+++ app/devops/tests/unit/software_enable_feature_flag/test_unit_software_enable_feature_flag_viewset.py
@@ -0,0 +1,73 @@
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+
+from rest_framework.reverse import reverse
+
+from access.models.organization import Organization
+
+from api.tests.abstract.viewsets import ViewSetModel
+
+from devops.viewsets.software_enable_feature_flag import ViewSet
+
+from itam.models.software import Software
+
+
+
+class ViewsetCommon(
+    ViewSetModel,
+):
+
+    viewset = ViewSet
+
+    route_name = 'v2:_api_v2_feature_flag_software'
+
+    @classmethod
+    def setUpTestData(self):
+        """Setup Test
+
+        1. Create an organization
+        3. create super user
+        """
+
+        organization = Organization.objects.create(name='test_org')
+
+        self.organization = organization
+
+        self.view_user = User.objects.create_user(username="test_view_user", password="password", is_superuser=True)
+
+        software = Software.objects.create(
+            organization = self.organization,
+            name = 'soft',
+        )
+
+        self.kwargs = { 'software_id': software.id }
+
+
+
+class ViewsetList(
+    ViewsetCommon,
+    TestCase,
+):
+
+
+    @classmethod
+    def setUpTestData(self):
+        """Setup Test
+
+        1. make list request
+        """
+
+
+        super().setUpTestData()
+
+
+        client = Client()
+        
+        url = reverse(
+            self.route_name + '-list',
+            kwargs = self.kwargs
+        )
+
+        client.force_login(self.view_user)
+
+        self.http_options_response_list = client.options(url)
