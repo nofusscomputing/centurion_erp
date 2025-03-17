bf973d3765a144ce91898904a64e07767d37d346
Jon <jonathon.lockwood@networkedweb.com>
2025-03-17 12:23:35 +0930

    fix(docker): only download feature flags when not a worker
    ref: #688 fixes #687

--- includes/entrypoint.sh
+++ includes/entrypoint.sh
@@ -59,21 +59,39 @@ if [ "$1" == "" ]; then
         fi;
 
 
+        echo '[info] Creating nginx service config';
+
+        cp /etc/supervisor/conf.source/cron.conf /etc/supervisor/conf.d/cron.conf;
+
+        if [ -f '/etc/supervisor/conf.d/cron.conf' ]; then
+
+
+            chmod 744 -R /etc/crontabs;
+
+            chown 0:0 -R /etc/crontabs;
+
+
+            echo '[info] CronD service config Created';
+
+        else
+
+            echo '[crit] CronD service config not created';
+
+        fi;
+
+
+        echo "[info] Fetching available feature flags";
+
+        export init_dir=${PWD}
+        
+        cd /app;
+        
+        python manage.py feature_flag;
+
     fi;
 
-    chmod 744 -R /etc/crontabs;
-
-    chown 0:0 -R /etc/crontabs;
 
     echo "[info] SupervisorD Setup successfully"
-
-    echo "[info] Fetching available feature flags";
-
-    export init_dir=${PWD}
-    
-    cd /app;
-    
-    python manage.py feature_flag;
 
     echo "[info] Starting SupervisorCTL";
 
