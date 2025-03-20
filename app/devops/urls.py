d006da803fdcf29f676fcbc530a31fa460241ed3
Jon <jonathon.lockwood@networkedweb.com>
2025-03-20 15:23:02 +0930

    feat(devops): Git Group Notes ViewSet
    ref: #693 #515

--- app/devops/urls.py
+++ app/devops/urls.py
@@ -19,6 +19,7 @@ router.register('feature_flag/(?P<model_id>[0-9]+)/notes', feature_flag_notes.Vi
 router.register(r'git_repository/(?P<git_provider>gitlab|github)?', git_repository.ViewSet, feature_flag = '2025-00001', basename='_api_v2_git_repository')
 
 router.register('git_group', git_group.ViewSet, feature_flag = '2025-00001', basename='_api_v2_git_group')
+router.register('git_group/notes', git_group_notes.ViewSet, feature_flag = '2025-00001', basename='_api_v2_git_group_notes')
 
 router.register('git_repository/github/notes', github_repository_notes.ViewSet, feature_flag = '2025-00001', basename='_api_v2_github_repository_notes')
 router.register('git_repository/gitlab/notes', gitlab_repository_notes.ViewSet, feature_flag = '2025-00001', basename='_api_v2_gitlab_repository_notes')
