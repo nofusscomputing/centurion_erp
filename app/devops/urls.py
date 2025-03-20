4c6c27a4bdc8bec6577c8f1e9ed1000d37ed793d
Jon <jonathon.lockwood@networkedweb.com>
2025-03-20 14:31:24 +0930

    feat(devops): [2025-00001] Git Group and Repositories URLs
    behind FF 2025-00001
    
    ref: #693 #515

--- app/devops/urls.py
+++ app/devops/urls.py
@@ -1,8 +1,10 @@
-from rest_framework.routers import DefaultRouter
+from centurion_feature_flag.urls.routers import DefaultRouter
 
 from devops.viewsets import (
     feature_flag,
     feature_flag_notes,
+    git_group,
+    git_repository,
 )
 
 
@@ -13,5 +15,9 @@ router = DefaultRouter(trailing_slash=False)
 
 router.register('feature_flag', feature_flag.ViewSet, basename='_api_v2_feature_flag')
 router.register('feature_flag/(?P<model_id>[0-9]+)/notes', feature_flag_notes.ViewSet, basename='_api_v2_feature_flag_note')
+
+router.register(r'git_repository/(?P<git_provider>gitlab|github)?', git_repository.ViewSet, feature_flag = '2025-00001', basename='_api_v2_git_repository')
+
+router.register('git_group', git_group.ViewSet, feature_flag = '2025-00001', basename='_api_v2_git_group')
 
 urlpatterns = router.urls
