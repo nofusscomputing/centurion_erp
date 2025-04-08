6d708d0c00e05f46dedbc5ae5a0f467f633444c2
Jon <jonathon.lockwood@networkedweb.com>
2025-04-08 21:55:10 +0930

    test(access): Unit Test for Contact model
    ref: #713 #703

---
+++ app/access/tests/unit/contact/test_unit_contact_model.py
@@ -0,0 +1,104 @@
+from django.db.models.fields import NOT_PROVIDED
+from django.test import TestCase
+
+from access.models.contact import Contact
+from access.tests.unit.person.test_unit_person_model import (
+    PersonModelInheritedCases
+)
+
+
+
+class ModelTestCases(
+    PersonModelInheritedCases,
+):
+
+    model = Contact
+
+    kwargs_item_create: dict = {
+        'email': 'ipweird@unit.test',
+    }
+
+
+
+    def test_model_field_directory_optional(self):
+        """Test Field
+
+        Field `dob` must be an optional field
+        """
+
+        assert self.model._meta.get_field('directory').blank
+
+
+    def test_model_field_directory_optional_default(self):
+        """Test Field
+
+        Field `directory` default value is `True`
+        """
+
+        assert (
+            self.model._meta.get_field('directory').default is True
+            and self.model._meta.get_field('directory').null is False
+        )
+
+
+    def test_model_field_email_mandatory(self):
+        """Test Field
+
+        Field `email` must be a mandatory field
+        """
+
+        assert(
+            not (
+                self.model._meta.get_field('email').blank
+                and self.model._meta.get_field('email').null
+            )
+            and self.model._meta.get_field('email').default is NOT_PROVIDED
+        )
+
+
+    # def test_model_field_l_name_mandatory(self):
+    #     """Test Field
+
+    #     Field `l_name` must be a mandatory field
+    #     """
+
+    #     assert (
+    #         not (
+    #             self.model._meta.get_field('l_name').blank
+    #             and self.model._meta.get_field('l_name').null
+    #         )
+    #         and self.model._meta.get_field('l_name').default is NOT_PROVIDED
+    #     )
+
+
+
+class ContactModelInheritedCases(
+    ModelTestCases,
+):
+    """Sub-Entity Test Cases
+
+    Test Cases for Entity models that inherit from model Contact
+    """
+
+    kwargs_item_create: dict = None
+
+    model = None
+
+
+    @classmethod
+    def setUpTestData(self):
+
+        self.kwargs_item_create.update(
+            super().kwargs_item_create
+        )
+
+        super().setUpTestData()
+
+
+
+class ContactModelTest(
+    ModelTestCases,
+    TestCase,
+):
+
+    pass
