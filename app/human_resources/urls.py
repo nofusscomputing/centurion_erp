529a7fa3855f5c0fd689c122ecbe51da3f36a370
Jon <jonathon.lockwood@networkedweb.com>
2025-04-15 15:45:43 +0930

    chore(human_resources): add employee to feature flag `2025-00005`
    ref: #722 #684 #92

--- app/human_resources/urls.py
+++ app/human_resources/urls.py
@@ -1,4 +1,4 @@
-from rest_framework.routers import DefaultRouter
+from centurion_feature_flag.urls.routers import DefaultRouter
 
 
 app_name = "hr"
@@ -7,6 +7,6 @@ from human_resources.viewsets import index as HumanResourcesHome
 
 router = DefaultRouter(trailing_slash=False)
 
-router.register('', HumanResourcesHome.Index, basename='_api_v2_access_home')
+router.register('', HumanResourcesHome.Index, feature_flag = '2025-00005', basename='_api_v2_access_home')
 
 urlpatterns = router.urls
