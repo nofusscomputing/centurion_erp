6b4bd4db35a7c36c2857caa7299bf0db3bc8b779
Jon <jonathon.lockwood@networkedweb.com>
2025-04-15 15:20:48 +0930

    feat(human_resources): New model Employee
    ref: #722 #684

---
+++ app/human_resources/models/employee.py
@@ -0,0 +1,109 @@
+from django.db import models
+
+from access.models.contact import Contact
+
+
+
+class Employee(
+    Contact
+):
+
+
+    class Meta:
+
+        ordering = [
+            'email',
+        ]
+
+        verbose_name = 'Employee'
+
+        verbose_name_plural = 'Employees'
+
+
+    employee_number = models.IntegerField(
+        blank = False,
+        help_text = 'Employees identification number.',
+        null = False,
+        unique = True,
+        verbose_name = 'Employee Number'
+    )
+
+
+    def __str__(self) -> str:
+
+        return self.f_name + ' ' + self.l_name
+
+    documentation = ''
+
+    page_layout: list = [
+        {
+            "name": "Details",
+            "slug": "details",
+            "sections": [
+                {
+                    "layout": "double",
+                    "left": [
+                        'organization',
+                        'created',
+                        'modified',
+                    ],
+                    "right": [
+                        'model_notes',
+                        'directory',
+                    ]
+                },
+                {
+                    "name": "Personal Details",
+                    "layout": "double",
+                    "left": [
+                        'employee_number',
+                        'display_name',
+                        'dob',
+                    ],
+                    "right": [
+                        'f_name',
+                        'm_name',
+                        'l_name',
+                    ]
+                },
+                {
+                    "name": "",
+                    "layout": "double",
+                    "left": [
+                        'email',
+                    ],
+                    "right": [
+                        '',
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Knowledge Base",
+            "slug": "kb_articles",
+            "sections": [
+                {
+                    "layout": "table",
+                    "field": "knowledge_base",
+                }
+            ]
+        },
+        {
+            "name": "Notes",
+            "slug": "notes",
+            "sections": []
+        },
+    ]
+
+    table_fields: list = [
+        {
+            "field": "employee_number",
+            "type": "link",
+            "key": "_self"
+        },
+        'f_name',
+        'l_name',
+        'email',
+        'organization',
+        'created',
+    ]
---
+++ docs/projects/centurion_erp/user/human_resources/employee.md
@@ -0,0 +1,14 @@
+---
+title: Employee
+description: Employee as part of Human Resources User Documentation for Centurion ERP by No Fuss Computing
+date: 2025-04-15
+template: project.html
+about: https://github.com/nofusscomputing/centurion_erp
+---
+
+This component within HR is for the management of an organizations employee(s).
+
+
+## Fields
+
+- employee number _An internal number to reference the employee_
