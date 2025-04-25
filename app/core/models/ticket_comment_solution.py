33362a28d5fb5a79c29e8910a2c700a344a79cfe
Jon <jonathon.lockwood@networkedweb.com>
2025-04-26 06:21:04 +0930

    feat(core): New model TicketCommentSolution
    ref: #732 #728

--- app/core/models/ticket_comment_solution.py
+++ app/core/models/ticket_comment_solution.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 from core import exceptions as centurion_exception
 from core.models.ticket_comment_base import TicketCommentBase
 
@@ -22,6 +25,31 @@ class TicketCommentSolution(
 
     def clean(self):
 
+        if self.ticket.is_solved:
+
+            raise centurion_exception.ValidationError(
+                detail = 'Ticket is already solved',
+                code = 'ticket_already_solved'
+            )
+
+
         super().clean()
+
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+
+        self.is_closed = True
+
+        self.date_closed = datetime.datetime.now(tz=datetime.timezone.utc).replace(microsecond=0).isoformat()
+
+        self.ticket.is_solved = self.is_closed
+
+        self.ticket.date_solved = self.date_closed
+
+        self.ticket.status = self.ticket.TicketStatus.SOLVED
+
+        self.ticket.save()
+
+        super().save(force_insert = force_insert, force_update = force_update, using = using, update_fields = update_fields)
 
 
