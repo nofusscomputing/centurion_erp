ea38665dab5bb6efc66608351750250ba2cf6e71
Jon <jonathon.lockwood@networkedweb.com>
2025-04-29 16:13:59 +0930

    test(itim): SLMTicket Initial Test Suite for Serializer
    ref: #733 #727

---
+++ app/itim/tests/functional/ticket_slm/test_functional_ticket_slm_serializer.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+
+from core.tests.functional.ticket_base.test_functional_ticket_base_serializer import TicketBaseSerializerInheritedCases
+
+from itim.serializers.ticket_slm import (
+    SLMTicket,
+    ModelSerializer
+)
+
+
+
+class SerializerTestCases(
+    TicketBaseSerializerInheritedCases,
+):
+
+    model = SLMTicket
+
+    create_model_serializer = ModelSerializer
+
+    valid_data: dict = {
+        'ttr': 2,
+        'ttr': 3,
+    }
+
+
+
+class SLMTicketSerializerInheritedCases(
+    SerializerTestCases,
+):
+
+    kwargs_create_item: dict = {}
+    """ Model kwargs to create item"""
+
+    model = None
+    """Model to test"""
+
+    valid_data: dict = {}
+    """Valid data used by serializer to create object"""
+
+
+    @classmethod
+    def setUpTestData(self):
+
+        self.valid_data = {
+            **super().valid_data,
+            **self.valid_data
+        }
+
+        super().setUpTestData()
+
+
+
+class SLMTicketSerializerTest(
+    SerializerTestCases,
+    TestCase,
+):
+
+    pass
