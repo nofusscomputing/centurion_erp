a898549469cc0a921b55294bef8cb51fba571b50
Jon <jonathon.lockwood@networkedweb.com>
2025-05-01 01:23:40 +0930

    test(itim): RequestTicket Updated, yet incomplete Test Suite for Serializer
    ref: #733 #725

--- app/itim/tests/functional/ticket_request/test_functional_ticket_request_serializer.py
+++ app/itim/tests/functional/ticket_request/test_functional_ticket_request_serializer.py
@@ -1,49 +1,29 @@
-from django.test import TestCase
-
-from itim.serializers.ticket_request import (
-    RequestTicket,
-    ModelSerializer
-)
 from itim.tests.functional.ticket_slm.test_functional_ticket_slm_serializer import SLMTicketSerializerInheritedCases
 
 
 
-class SerializerTestCases(
+class RequestTicketSerializerTestCases(
     SLMTicketSerializerInheritedCases,
 ):
 
-    model = RequestTicket
-
-    create_model_serializer = ModelSerializer
+    pass
 
 
 
-class TicketBaseSerializerInheritedCases(
-    SerializerTestCases,
+class RequestTicketSerializerInheritedCases(
+    RequestTicketSerializerTestCases,
 ):
 
     model = None
     """Model to test"""
 
-    valid_data: dict = {}
+    valid_data: dict = None
     """Valid data used by serializer to create object"""
 
 
-    @classmethod
-    def setUpTestData(self):
 
-        self.valid_data = {
-            **super().valid_data,
-            **self.valid_data
-        }
-
-        super().setUpTestData()
-
-
-
-class TicketBaseSerializerTest(
-    SerializerTestCases,
-    TestCase,
+class RequestTicketSerializerPyTest(
+    RequestTicketSerializerTestCases,
 ):
 
     pass
