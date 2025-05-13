c8df4b21a0e87660eceb0acf62d8816a326d2c7f
Jon <jonathon.lockwood@networkedweb.com>
2025-05-13 19:19:10 +0930

    feat(core): New model TicketCommentAction
    ref: #754 #736

---
+++ app/core/models/ticket_comment_action.py
@@ -0,0 +1,55 @@
+import datetime
+
+
+from core import exceptions as centurion_exception
+from core.models.ticket_comment_base import TicketCommentBase
+
+
+
+class TicketCommentAction(
+    TicketCommentBase,
+):
+
+    class Meta:
+
+        ordering = [
+            'id'
+        ]
+
+        permissions = [
+            ('import_ticketcommentaction', 'Can import ticket action comment.'),
+        ]
+
+        sub_model_type = 'action'
+
+        verbose_name = "Ticket Comment Action"
+
+        verbose_name_plural = "Ticket Comment Actions"
+
+
+    def clean(self):
+
+        self.is_closed = True
+
+        super().clean()
+
+
+
+    # def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+
+    #     super().save(force_insert = force_insert, force_update = force_update, using = using, update_fields = update_fields)
+
+    #     self.ticket.is_solved = 
+
+    #     self.ticket.date_solved = self.date_closed
+
+    #     self.ticket.status = self.ticket.TicketStatus.SOLVED
+
+    #     self.ticket.save()
+
+    #     # clear comment cache
+    #     if hasattr(self.ticket, '_ticket_comments'):
+
+    #         del self.ticket._ticket_comments
+
+
