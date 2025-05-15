c39f479b96955973deeacc77b4c25a27eba3311d
Jon <jonathon.lockwood@networkedweb.com>
2025-05-16 03:07:46 +0930

    refactor(Access): Update Functional ViewSet to use PyTest for Contact Model
    ref: #761 #730

--- app/access/tests/functional/contact/test_functional_contact_viewset.py
+++ app/access/tests/functional/contact/test_functional_contact_viewset.py
@@ -2,91 +2,28 @@ from django.test import TestCase
 
 from access.models.contact import Contact
 from access.tests.functional.person.test_functional_person_viewset import (
-    PersonMetadataInheritedCases,
-    PersonPermissionsAPIInheritedCases,
     PersonViewSetInheritedCases
 )
 
 
 
-class ViewSetBase:
-
-    add_data = {
-        'email': 'ipfunny@unit.test',
-    }
-
-    kwargs_create_item_diff_org = {
-        'email': 'ipstrange@unit.test',
-    }
-
-    kwargs_create_item = {
-        'email': 'ipweird@unit.test',
-    }
-
-    model = Contact
-
-    url_kwargs: dict = {}
-
-    url_view_kwargs: dict = {}
-
-
-
-class PermissionsAPITestCases(
-    ViewSetBase,
-    PersonPermissionsAPIInheritedCases,
-):
-
-    pass
-
-
-
-class ContactPermissionsAPIInheritedCases(
-    PermissionsAPITestCases,
-):
-
-    add_data: dict = None
-
-    model = None
-
-    kwargs_create_item: dict = None
-
-    kwargs_create_item_diff_org: dict = None
-
-
-    @classmethod
-    def setUpTestData(self):
-
-        self.add_data.update(
-            super().add_data
-        )
-
-        self.kwargs_create_item.update(
-            super().kwargs_create_item
-        )
-
-        self.kwargs_create_item_diff_org.update(
-            super().kwargs_create_item_diff_org
-        )
-
-        super().setUpTestData()
-
-
-
-class ContactPermissionsAPITest(
-    PermissionsAPITestCases,
-    TestCase,
-):
-
-    pass
-
-
-
 class ViewSetTestCases(
-    ViewSetBase,
     PersonViewSetInheritedCases,
 ):
 
-    pass
+    add_data: dict = {
+        'email': 'ipfunny@unit.test',
+    }
+
+    kwargs_create_item: dict = {
+        'email': 'ipweird@unit.test',
+    }
+
+    kwargs_create_item_diff_org: dict = {
+        'email': 'ipstrange@unit.test',
+    }
+
+    model = Contact
 
 
 
@@ -96,21 +33,19 @@ class ContactViewSetInheritedCases(
 
     model = None
 
-    kwargs_create_item: dict = None
-
-    kwargs_create_item_diff_org: dict = None
-
 
     @classmethod
     def setUpTestData(self):
 
-        self.kwargs_create_item.update(
-            super().kwargs_create_item
-        )
+        self.kwargs_create_item = {
+            **super().kwargs_create_item,
+            **self.kwargs_create_item
+        }
 
-        self.kwargs_create_item_diff_org.update(
-            super().kwargs_create_item_diff_org
-        )
+        self.kwargs_create_item_diff_org = {
+            **super().kwargs_create_item_diff_org,
+            **self.kwargs_create_item_diff_org
+        }
 
         super().setUpTestData()
 
@@ -120,50 +55,4 @@ class ContactViewSetTest(
     ViewSetTestCases,
     TestCase,
 ):
-
-    pass
-
-
-
-class MetadataTestCases(
-    ViewSetBase,
-    PersonMetadataInheritedCases,
-):
-
-    pass
-
-
-
-class ContactMetadataInheritedCases(
-    MetadataTestCases,
-):
-
-    model = None
-
-    kwargs_create_item: dict = None
-
-    kwargs_create_item_diff_org: dict = None
-
-
-    @classmethod
-    def setUpTestData(self):
-
-        self.kwargs_create_item.update(
-            super().kwargs_create_item
-        )
-
-        self.kwargs_create_item_diff_org.update(
-            super().kwargs_create_item_diff_org
-        )
-
-        super().setUpTestData()
-
-
-
-class ContactMetadataTest(
-    MetadataTestCases,
-    TestCase,
-
-):
-
     pass
