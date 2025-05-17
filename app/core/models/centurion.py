648a414f0e684096a18dbbcd43ff8735c7284467
Jon <jonathon.lockwood@networkedweb.com>
2025-05-17 21:05:57 +0930

    feat(core): Dynamic History model creation
    ref: #765 #758 #759

--- app/core/models/centurion.py
+++ app/core/models/centurion.py
@@ -11,6 +11,13 @@ class CenturionModel(
 
 
 
+    _audit_enabled: bool = True
+    """Should this model have audit history kept"""
+
+    _is_submodel: bool = False
+    """This model a sub-model"""
+
+
     class Meta:
 
         abstract = True
@@ -22,3 +29,27 @@ class CenturionModel(
         if value is None:
 
             raise ValidationError(code = 'field_value_not_none', message = 'Value can not be none.')
+
+
+
+    def get_history_model_name(self) -> str:
+        """Get the name for the History Model
+
+        Returns:
+            str: Name of the history model (`<model class name>AuditHistory`)
+        """
+        
+        return f'{self._meta.object_name}AuditHistory'
+
+
+class CenturionSubModel(
+    CenturionModel
+):
+
+    _is_submodel: bool = True
+    """This model a sub-model"""
+
+
+    class Meta:
+
+        abstract = True
