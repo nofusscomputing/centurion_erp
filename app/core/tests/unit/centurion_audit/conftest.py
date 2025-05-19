03e067761d6851edbbc7cef0687fbdb835d25f76
Jon <jonathon.lockwood@networkedweb.com>
2025-05-20 05:52:38 +0930

    revert(core): Relocate history model class
    reverted commit was 5cc08e3e94a33cf19a63cd716f4927b347477e1e
    
    ref: #765 #766

--- app/core/tests/unit/centurion_audit/conftest.py
+++ app/core/tests/unit/centurion_audit/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 
-from core.models.audit import ModelHistory
+from core.models.audit import CenturionAudit
 
 
 
 @pytest.fixture( scope = 'class')
 def model(request):
 
-    request.cls.model = ModelHistory
+    request.cls.model = CenturionAudit
 
     yield request.cls.model
 
