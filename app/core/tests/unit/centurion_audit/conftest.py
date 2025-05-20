b6f1f93c8c96afac11b0e271fb555b15ff2fb6e2
Jon <jonathon.lockwood@networkedweb.com>
2025-05-21 01:42:36 +0930

    chore: comment out test setup for skeleton test suites not in use yet
    ref: #770

--- app/core/tests/unit/centurion_audit/conftest.py
+++ app/core/tests/unit/centurion_audit/conftest.py
@@ -1,14 +1,14 @@
-import pytest
+# import pytest
 
-from core.models.audit import CenturionAudit
+# from core.models.audit import CenturionAudit
 
 
 
-@pytest.fixture( scope = 'class')
-def model(request):
+# @pytest.fixture( scope = 'class')
+# def model(request):
 
-    request.cls.model = CenturionAudit
+#     request.cls.model = CenturionAudit
 
-    yield request.cls.model
+#     yield request.cls.model
 
-    del request.cls.model
+#     del request.cls.model
