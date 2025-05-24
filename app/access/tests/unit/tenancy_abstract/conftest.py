bc1f12c47feb987cc264310638a98be2de37bc25
Jon <jonathon.lockwood@networkedweb.com>
2025-05-25 09:02:15 +0930

    feat(core): Add model instance to history object during history creation
    ref: #772 #759

--- app/access/tests/unit/tenancy_abstract/conftest.py
+++ app/access/tests/unit/tenancy_abstract/conftest.py
@@ -7,8 +7,4 @@ from access.models.tenancy_abstract import TenancyAbstractModel
 @pytest.fixture( scope = 'class')
 def model(request):
 
-    request.cls.model = TenancyAbstractModel
-
-    yield request.cls.model
-
-    del request.cls.model
+    yield TenancyAbstractModel
