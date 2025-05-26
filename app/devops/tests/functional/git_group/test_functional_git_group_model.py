8aece83b3679a5ae367e11be58e78debbe353e6c
Jon <jonathon.lockwood@networkedweb.com>
2025-05-26 14:31:15 +0930

    test(devops): Ensure that a Github group cant have a parent/"be nested"
    ref: #773 #515 #249

--- app/devops/tests/functional/git_group/test_functional_git_group_model.py
+++ app/devops/tests/functional/git_group/test_functional_git_group_model.py
@@ -1,5 +1,9 @@
 import pytest
 
+from django.core.exceptions import (
+    ValidationError
+)
+
 from core.tests.functional.centurion_abstract.test_functional_centurion_abstract_model import (
     CenturionAbstractModelInheritedCases
 )
@@ -34,6 +38,8 @@ class GitGroupModelTestCases(
 
         kwargs_create_item = self.kwargs_create_item.copy()
 
+        kwargs_create_item['provider'] = model.GitProvider.GITLAB
+
         del kwargs_create_item['organization']
         kwargs_create_item['parent_group'] = created_model
 
@@ -46,6 +52,33 @@ class GitGroupModelTestCases(
         child_group.delete()
 
         assert child_group.organization == created_model.organization
+
+
+
+    def test_model_create_with_parent_exception_github(self, created_model, model):
+        """Model Created
+
+        Ensure that the model when created with a parent git group, with the
+        provider being Github, that an exception is thrown as Github groups
+        can't have parents/nesting.
+        """
+
+        kwargs_create_item = self.kwargs_create_item.copy()
+
+        kwargs_create_item['provider'] = model.GitProvider.GITHUB
+
+        del kwargs_create_item['organization']
+        kwargs_create_item['parent_group'] = created_model
+
+        with pytest.raises( ValidationError ) as e:
+
+            child_group = model.objects.create(
+                **kwargs_create_item
+            )
+
+            child_group.delete()
+
+        assert e.value.error_dict['__all__'][0].code == 'no_parent_for_github_group'
 
 
 
