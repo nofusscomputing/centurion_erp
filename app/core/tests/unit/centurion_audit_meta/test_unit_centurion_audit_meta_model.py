c9cdd1d69c0d9699c159593979f98379750ad7bb
Jon <jonathon.lockwood@networkedweb.com>
2025-05-28 18:29:20 +0930

    test(core): Dynamic Unit Test Suites for Meta Models AuditHistory
    ref: #775 #759 #767

--- app/core/tests/unit/centurion_audit_meta/test_unit_centurion_audit_meta_model.py
+++ app/core/tests/unit/centurion_audit_meta/test_unit_centurion_audit_meta_model.py
@@ -71,7 +71,75 @@ class MetaAbstractModelInheritedCases(
     MetaAbstractModelTestCases,
 ):
 
-    pass
+
+    @pytest.mark.xfail( reason = 'This model does not require a tag')
+    def test_model_tag_defined(self, model):
+        """ Model Tag
+
+        Ensure that the model has a tag defined.
+        """
+
+        assert model.model_tag is not None
+
+
+    def test_method_get_url_attribute__is_submodel_set(self, mocker, model_instance, audit_model):
+        """Test Class Method
+        
+        Ensure method `get_url` calls reverse
+        """
+
+        site_path = '/module/page/1'
+
+        assert model_instance._is_submodel    # Test Failsafe. Confirm state
+
+        reverse = mocker.patch('rest_framework.reverse._reverse', return_value = site_path)
+
+        instance = audit_model()
+        instance.id = 1
+
+        model_instance.id = 1
+        model_instance.model = instance
+
+        url_basename = f'v2:_api_centurionaudit_sub-detail'
+
+        url = model_instance.get_url( relative = True)
+
+        reverse.assert_called_with(
+            url_basename,
+            None,
+            {
+                'app_label': model_instance._meta.app_label,
+                'model_name': str(model_instance._meta.model_name).replace('audithistory', ''),
+                'model_id': 1,
+                'pk': 1
+            },
+            None,
+            None
+        )
+
+
+
+    def test_method_get_url_kwargs(self, mocker, model_instance, audit_model):
+        """Test Class Method
+        
+        Ensure method `get_url_kwargs` returns the correct value.
+        """
+
+        instance = audit_model()
+        instance.id = 1
+
+        model_instance.id = 1
+        model_instance.model = instance
+
+        url = model_instance.get_url_kwargs()
+
+        assert model_instance.get_url_kwargs() == {
+            'app_label': model_instance._meta.app_label,
+            'model_name': str(model_instance._meta.model_name).replace('audithistory', ''),
+            'model_id': model_instance.model.id,
+            'pk': model_instance.id,
+        }
+
 
 
 
