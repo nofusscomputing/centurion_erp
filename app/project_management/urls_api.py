e04869cc6521c22d283d1c3e442795de0a8b33ce
Jon <jonathon.lockwood@networkedweb.com>
2025-05-29 10:29:21 +0930

    refactor(project_management): move url routes from core.urls to own module `urls_api.py`
    ref: #776 #777

---
+++ app/project_management/urls_api.py
@@ -0,0 +1,50 @@
+from centurion_feature_flag.urls.routers import DefaultRouter
+
+from project_management.viewsets import (
+    index as project_management_v2,
+    project as project_v2,
+    project_milestone as project_milestone_v2,
+    project_milestone_notes,
+    project_notes,
+    project_task,
+)
+
+
+
+# app_name = "project_management"
+
+
+router: DefaultRouter = DefaultRouter(trailing_slash=False)
+
+
+router.register(
+    prefix = '', viewset = project_management_v2.Index,
+    basename = '_api_v2_project_management_home'
+)
+router.register(
+    prefix = 'project', viewset = project_v2.ViewSet,
+    basename = '_api_v2_project'
+)
+router.register(
+    prefix = 'project/(?P<project_id>[0-9]+)/milestone',
+    viewset = project_milestone_v2.ViewSet,
+    basename = '_api_v2_project_milestone'
+)
+router.register(
+    prefix = 'project/(?P<project_id>[0-9]+)/milestone/(?P<model_id>[0-9]+)/notes',
+    viewset = project_milestone_notes.ViewSet,
+    basename = '_api_v2_project_milestone_note'
+)
+router.register(
+    prefix = 'project/(?P<model_id>[0-9]+)/notes',
+    viewset = project_notes.ViewSet,
+    basename = '_api_v2_project_note'
+)
+router.register(
+    prefix = 'project/(?P<project_id>[0-9]+)/project_task',
+    viewset = project_task.ViewSet,
+    basename = '_api_v2_ticket_project_task'
+)
+
+
+urlpatterns = router.urls
