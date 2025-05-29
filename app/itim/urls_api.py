e74f30fd7be50b1647f0ce37a241e8fca282c265
Jon <jonathon.lockwood@networkedweb.com>
2025-05-29 10:22:30 +0930

    refactor(itim): move url routes from core.urls to own module `urls_api.py`
    ref: #776 #777

---
+++ app/itim/urls_api.py
@@ -0,0 +1,61 @@
+from centurion_feature_flag.urls.routers import DefaultRouter
+
+from itim.viewsets import (
+    index as itim_v2,
+    change,
+    cluster as cluster_v2,
+    cluster_notes,
+    incident,
+    problem,
+    service as service_v2,
+    service_cluster,
+    service_notes,
+)
+
+
+
+# app_name = "itim"
+
+
+router: DefaultRouter = DefaultRouter(trailing_slash=False)
+
+
+router.register(
+    prefix = '', viewset = itim_v2.Index,
+    basename = '_api_v2_itim_home'
+)
+router.register(
+    prefix = 'ticket/change', viewset = change.ViewSet,
+    basename = '_api_v2_ticket_change'
+)
+router.register(
+    prefix = 'cluster', viewset = cluster_v2.ViewSet,
+    basename = '_api_v2_cluster'
+)
+router.register(
+    prefix = 'cluster/(?P<cluster_id>[0-9]+)/service', viewset = service_cluster.ViewSet,
+    basename = '_api_v2_service_cluster'
+)
+router.register(
+    prefix = 'cluster/(?P<model_id>[0-9]+)/notes', viewset = cluster_notes.ViewSet,
+    basename = '_api_v2_cluster_note'
+)
+router.register(
+    prefix = 'ticket/incident', viewset = incident.ViewSet,
+    basename = '_api_v2_ticket_incident'
+)
+router.register(
+    prefix = 'ticket/problem', viewset = problem.ViewSet,
+    basename = '_api_v2_ticket_problem'
+)
+router.register(
+    prefix = 'service', viewset = service_v2.ViewSet,
+    basename = '_api_v2_service'
+)
+router.register(
+    prefix = 'service/(?P<model_id>[0-9]+)/notes', viewset = service_notes.ViewSet,
+    basename = '_api_v2_service_note'
+)
+
+
+urlpatterns = router.urls
