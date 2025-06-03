719b02dd2b6f99004eaf936feac78a2210d3089a
Jon <jonathon.lockwood@networkedweb.com>
2025-06-03 10:27:53 +0930

    chore(devops): Add FeatureFlag old history and notes models to init so they exist
    ref: #783 #782

--- app/devops/models/__init__.py
+++ app/devops/models/__init__.py
@@ -4,4 +4,6 @@ from . import git_group_history    # pylint: disable=W0611:unused-import
 from . import git_group_notes    # pylint: disable=W0611:unused-import
 from . import git_repository    # pylint: disable=W0611:unused-import
 from . import feature_flag    # pylint: disable=W0611:unused-import
+from . import feature_flag_history    # pylint: disable=W0611:unused-import
+from . import feature_flag_notes    # pylint: disable=W0611:unused-import
 from . import software_enable_feature_flag    # pylint: disable=W0611:unused-import
