d5b465ea0ed31affab4ac711f64a3e769779646b
Jon <jonathon.lockwood@networkedweb.com>
2025-06-03 10:27:11 +0930

    chore(devops): Add GitGroup old history and notes models to init so they exist
    ref: #783 #781

--- app/devops/models/__init__.py
+++ app/devops/models/__init__.py
@@ -1,5 +1,7 @@
 from . import check_ins    # pylint: disable=W0611:unused-import
 from . import git_group    # pylint: disable=W0611:unused-import
+from . import git_group_history    # pylint: disable=W0611:unused-import
+from . import git_group_notes    # pylint: disable=W0611:unused-import
 from . import git_repository    # pylint: disable=W0611:unused-import
 from . import feature_flag    # pylint: disable=W0611:unused-import
 from . import software_enable_feature_flag    # pylint: disable=W0611:unused-import
