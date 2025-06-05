3d961ecc9e4b8eea2d155ee82162bc27523d78c8
Jon <jonathon.lockwood@networkedweb.com>
2025-06-05 14:09:55 +0930

    chore(base): disable tests that require rewrite
    ref: #788 #735

--- app/centurion/urls.py
+++ app/centurion/urls.py
@@ -22,37 +22,17 @@ from django.urls import include, path, re_path
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-from .views import home
-
-from core.views import related_ticket, ticket_linked_item
-
-from settings.views import user_settings
-
 
 
 urlpatterns = [
-    path('', home.HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls, name='_administration'),
 
     path('account/password_change/', auth_views.PasswordChangeView.as_view(template_name="password_change.html.j2"), name="change_password"),
-    path('account/settings/<int:pk>', user_settings.View.as_view(), name="_settings_user"),
-    path('account/settings/<int:pk>/edit', user_settings.Change.as_view(), name="_settings_user_change"),
-    path('account/settings/<int:user_id>/token/add', user_settings.TokenAdd.as_view(), name="_user_auth_token_add"),
-    path('account/settings/<int:user_id>/token/<int:pk>/delete', user_settings.TokenDelete.as_view(), name="_user_auth_token_delete"),
-    path("account/", include("django.contrib.auth.urls")),
 
-    path("organization/", include("access.urls")),
-    path("assistance/", include("assistance.urls")),
-    path("itam/", include("itam.urls")),
-    path("itim/", include("itim.urls")),
-    path("config_management/", include("config_management.urls")),
+    path("account/", include("django.contrib.auth.urls")),
 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
-
-    path('ticket/<str:ticket_type>/<int:ticket_id>/relate/add', related_ticket.Add.as_view(), name="_ticket_related_add"),
-
-    path('ticket/<str:ticket_type>/<int:ticket_id>/linked_item/add', ticket_linked_item.Add.as_view(), name="_ticket_linked_item_add"),
 
 ]
 
@@ -89,12 +69,3 @@ if settings.DEBUG:
 
         path("__debug__/", include("debug_toolbar.urls"), name='_debug'),
     ]
-
-# must be after above
-urlpatterns += [
-
-    path("project_management/", include("project_management.urls")),
-
-    path("settings/", include("settings.urls")),
-
-]
