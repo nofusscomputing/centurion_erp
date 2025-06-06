fc60d7f3be7c92685f0c0408397fa88f2793b0b3
Jon <jonathon.lockwood@networkedweb.com>
2025-06-06 12:19:44 +0930

    feat(config_management): switch model ConfigGroupSoftware to inheirt from CenturionModel
    ref: #789 #795

--- app/config_management/urls_api.py
+++ app/config_management/urls_api.py
@@ -23,12 +23,12 @@ router.register(
 )
 router.register(
     prefix = 'group/(?P<parent_group>[0-9]+)/child_group', viewset = config_group_v2.ViewSet,
-    basename = '_api_v2_config_group_child'
+    basename = '_api_configgroups_child'
 )
 router.register(
     prefix = 'group/(?P<config_group_id>[0-9]+)/software',
     viewset = config_group_software_v2.ViewSet,
-    basename = '_api_v2_config_group_software'
+    basename = '_api_configgroupsoftware'
 )
 
 
