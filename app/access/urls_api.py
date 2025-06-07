b962004f07a18cf6a8fbc8cc3f471a253b583255
Jon <jonathon.lockwood@networkedweb.com>
2025-06-07 19:56:28 +0930

    revert(access): revert model inheriteance work
    ref: #804 #791

--- app/access/urls_api.py
+++ app/access/urls_api.py
@@ -7,7 +7,7 @@ from access.viewsets import (
     index as access_v2,
     organization as organization_v2,
     role,
-    team,
+    team as team_v2,
     team_user as team_user_v2
 )
 
@@ -64,8 +64,8 @@ router.register(
 # )
 
 router.register(
-    prefix = 'tenant/(?P<organization_id>[0-9]+)/team', viewset = team.ViewSet,
-    basename = '_api_team'
+    prefix = 'tenant/(?P<organization_id>[0-9]+)/team', viewset = team_v2.ViewSet,
+    basename = '_api_v2_organization_team'
 )
 
 # router.register(
