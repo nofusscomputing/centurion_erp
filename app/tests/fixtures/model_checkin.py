237778100bc93457c771dfcd362963bc12ae704d
Jon <jonathon.lockwood@networkedweb.com>
2025-06-10 12:13:19 +0930

    feat(devops): Update checkin model fixture so it creates the feature flag
    ref: #813 #808

--- app/tests/fixtures/model_checkin.py
+++ app/tests/fixtures/model_checkin.py
@@ -13,29 +13,15 @@ def model_checkin():
 
 @pytest.fixture( scope = 'class')
 def kwargs_checkin(django_db_blocker,
-    kwargs_centurionmodel, model_software
+    kwargs_centurionmodel, kwargs_featureflag
 ):
 
-
-    with django_db_blocker.unblock():
-
-        random_str = str(datetime.datetime.now(tz=datetime.timezone.utc))
-
-        software = model_software.objects.create(
-            organization = kwargs_centurionmodel['organization'],
-            name = 'ci' + random_str
-        )
-
-        kwargs = {
-            **kwargs_centurionmodel.copy(),
-            'software': software,
-            'version': '1.20.300',
-            'deployment_id': 'rand deploymentid',
-            'feature': 'a feature',
-        }
+    kwargs = {
+        **kwargs_centurionmodel.copy(),
+        'software': kwargs_featureflag['software'],
+        'version': '1.20.300',
+        'deployment_id': 'rand deploymentid',
+        'feature': 'a feature',
+    }
 
     yield kwargs.copy()
-
-    with django_db_blocker.unblock():
-
-        software.delete()
