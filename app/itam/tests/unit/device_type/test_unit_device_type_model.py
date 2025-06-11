febcf7a128e9c9fef5a2ac3e8a4e018d38bef2e0
Jon <jonathon.lockwood@networkedweb.com>
2025-06-11 17:13:02 +0930

    refactor(itam): Updated Unit model test suite for DeviceType model
    ref:#817 closes #803

---
+++ app/itam/tests/unit/device_type/test_unit_device_type_model.py
@@ -0,0 +1,63 @@
+import pytest
+
+from django.db import models
+
+
+from core.tests.unit.centurion_abstract.test_unit_centurion_abstract_model import (
+    CenturionAbstractModelInheritedCases
+)
+
+
+
+@pytest.mark.model_devicetype
+class DeviceTypeModelTestCases(
+    CenturionAbstractModelInheritedCases
+):
+
+
+    @property
+    def parameterized_class_attributes(self):
+        
+        return {
+            'model_tag': {
+                'type': str,
+                'value': 'device_type'
+            },
+        }
+
+
+    @property
+    def parameterized_model_fields(self):
+        
+        return {
+        'name': {
+            'blank': False,
+            'default': models.fields.NOT_PROVIDED,
+            'field_type': models.IntegerField,
+            'length': 50,
+            'null': False,
+            'unique': True,
+        },
+        'modified': {
+            'blank': False,
+            'default': models.fields.NOT_PROVIDED,
+            'field_type': models.DateTimeField,
+            'null': False,
+            'unique': False,
+        },
+    }
+
+
+
+class DeviceTypeModelInheritedCases(
+    DeviceTypeModelTestCases,
+):
+    pass
+
+
+
+@pytest.mark.module_itam
+class DeviceTypeModelPyTest(
+    DeviceTypeModelTestCases,
+):
+    pass
