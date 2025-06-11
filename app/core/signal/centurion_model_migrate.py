cfa2f81b0da30f67a269f082e2e122f949a8890d
Jon <jonathon.lockwood@networkedweb.com>
2025-06-11 15:57:26 +0930

    feat(itam): Add DeviceModel for history and notes data migration
    ref:#817 #800

--- app/core/signal/centurion_model_migrate.py
+++ app/core/signal/centurion_model_migrate.py
@@ -112,6 +112,12 @@ def centurion_model_migrate(sender, **kwargs):
             'model_name': 'Device',
             'history_model_name': 'DeviceHistory',
             'notes_model_name': 'DeviceNotes'
+        },
+        {
+            'app_label': 'itam',
+            'model_name': 'DeviceModel',
+            'history_model_name': 'DeviceModelHistory',
+            'notes_model_name': 'DeviceModelNotes'
         }
     ]
 
