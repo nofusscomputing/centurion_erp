0aa1e495057a7181c8df83a2ad5a68d6bb31612b
Jon <jonathon.lockwood@networkedweb.com>
2025-06-16 12:55:19 +0930

    refactor(settings): Update URL route name for AppSettings model
    ref: #833 #834

---
+++ app/settings/tests/unit/app_settings/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+
+@pytest.fixture( scope = 'class')
+def model(model_appsettings):
+
+    yield model_appsettings
+
+
+@pytest.fixture( scope = 'class', autouse = True)
+def model_kwargs(request, kwargs_appsettings):
+
+    request.cls.kwargs_create_item = kwargs_appsettings.copy()
+
+    yield kwargs_appsettings.copy()
+
+    if hasattr(request.cls, 'kwargs_create_item'):
+        del request.cls.kwargs_create_item
