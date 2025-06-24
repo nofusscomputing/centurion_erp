bf1bb23dd58cc9685b369c788123fd32ef127271
Jon <jonathon.lockwood@networkedweb.com>
2025-06-24 15:11:53 +0930

    chore(test): Add required fields to service fixture
    ref: #849 #828

--- app/tests/fixtures/model_service.py
+++ app/tests/fixtures/model_service.py
@@ -14,7 +14,8 @@ def model_service():
 @pytest.fixture( scope = 'class')
 def kwargs_service(django_db_blocker,
     kwargs_centurionmodel,
-    kwargs_device, model_device
+    kwargs_device, model_device,
+    kwargs_port, model_port,
 ):
 
     random_str = str(datetime.datetime.now(tz=datetime.timezone.utc))
@@ -30,9 +31,14 @@ def kwargs_service(django_db_blocker,
 
         device = model_device.objects.create( **kwargs )
 
+        port = model_port.objects.create( **kwargs_port )
+
     kwargs = {
         **kwargs_centurionmodel.copy(),
         'name': 'service_' + random_str,
+        'device': device,
+        'config_key_variable': 'svc',
+        'port': port,
     }
 
     yield kwargs.copy()
@@ -40,3 +46,5 @@ def kwargs_service(django_db_blocker,
     with django_db_blocker.unblock():
 
         device.delete()
+
+        port.delete()
