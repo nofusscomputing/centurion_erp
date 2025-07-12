953dce29843ac7c6235f959fc89cbc1e093e3c6e
Jon <jonathon.lockwood@networkedweb.com>
2025-07-12 16:44:43 +0930

    feat(access): Add AuditHistory Serializer for Role model
    ref: #862 #844

--- app/access/models/__init__.py
+++ app/access/models/__init__.py
@@ -1,4 +1,3 @@
 from .organization_history import OrganizationHistory    # pylint: disable=W0611:unused-import
-from .role_history import RoleHistory    # pylint: disable=W0611:unused-import
 
 from .organization_notes import OrganizationNotes    # pylint: disable=W0611:unused-import
