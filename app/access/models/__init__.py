e0c8ef46f59ad2d62ddec842b330cf711b6d8ff3
Jon <jonathon.lockwood@networkedweb.com>
2025-07-12 16:43:58 +0930

    feat(access): Add Notes Serializer for Role model
    ref: #862 #844

--- app/access/models/__init__.py
+++ app/access/models/__init__.py
@@ -2,4 +2,3 @@ from .organization_history import OrganizationHistory    # pylint: disable=W0611
 from .role_history import RoleHistory    # pylint: disable=W0611:unused-import
 
 from .organization_notes import OrganizationNotes    # pylint: disable=W0611:unused-import
-from .role_notes import RoleNotes    # pylint: disable=W0611:unused-import
