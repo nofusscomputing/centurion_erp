e3b9cba23c01c6ba4428434c62fe26457e8fa507
Jon <jonathon.lockwood@networkedweb.com>
2025-07-30 08:27:50 +0930

    refactor(config_management): Model Unit Test Suite re-written to pytest for model ConfigGroup
    ref: #908 closes #905

--- app/config_management/tests/unit/config_groups/test_unit_config_groups_model.py
+++ app/config_management/tests/unit/config_groups/test_unit_config_groups_model.py
@@ -2,42 +2,71 @@ import pytest
 
 from django.db import models
 
-
 from core.tests.unit.centurion_abstract.test_unit_centurion_abstract_model import (
     CenturionAbstractModelInheritedCases
 )
 
 
 
-# class Old:
-
-#     kwargs_item_create = {
-#         'name': 'one',
-#         'config': dict({"key": "one", "existing": "dont_over_write"})
-#     }
-
-#     model = ConfigGroups
-
-#     @classmethod
-#     def setUpTestData(self):
-#         """Setup Test
-
-#         1. Create an organization for user and item
-#         2. Create an item
-
-#         """
-
-#         self.organization = Organization.objects.create(name='test_org')
-
-#         super().setUpTestData()
+@pytest.mark.model_configgroups
+class ConfigGroupModelTestCases(
+    CenturionAbstractModelInheritedCases
+):
 
 
-#         self.second_item = self.model.objects.create(
-#             organization = self.organization,
-#             name = 'one_two',
-#             config = dict({"key": "two"}),
-#             parent = self.item
-#         )
+    @property
+    def parameterized_class_attributes(self):
+
+        return {
+            'model_tag': {
+                'type': str,
+                'value': 'config_group'
+            },
+        }
+
+
+    @property
+    def parameterized_model_fields(self):
+
+        return {
+        'parent': {
+            'blank': True,
+            'default': models.fields.NOT_PROVIDED,
+            'field_type': models.ForeignKey,
+            'null': True,
+            'unique': False,
+        },
+        'name': {
+            'blank': False,
+            'default': models.fields.NOT_PROVIDED,
+            'field_type': models.CharField,
+            'max_length': 50,
+            'null': False,
+            'unique': False,
+        },
+        'config': {
+            'blank': True,
+            'default': models.fields.NOT_PROVIDED,
+            'field_type': models.JSONField,
+            'null': True,
+            'unique': False,
+        },
+        'hosts': {
+            'blank': True,
+            'default': models.fields.NOT_PROVIDED,
+            'field_type': models.ManyToManyField,
+            'null': False,
+            'unique': False,
+        },
+        'modified': {
+            'blank': False,
+            'default': models.fields.NOT_PROVIDED,
+            'field_type': models.DateTimeField,
+            'null': False,
+            'unique': False,
+        },
+    }
+
 
 #     def test_config_groups_count_child_groups(self):
 #         """ Test function count_children """
@@ -83,67 +112,6 @@ from core.tests.unit.centurion_abstract.test_unit_centurion_abstract_model impor
 #     def test_config_groups_config_keys_valid_ansible_variable():
 #         """ All config keys must be valid ansible variables """
 #         pass
-
-
-
-@pytest.mark.model_configgroups
-class ConfigGroupModelTestCases(
-    CenturionAbstractModelInheritedCases
-):
-
-
-    @property
-    def parameterized_class_attributes(self):
-
-        return {
-            'model_tag': {
-                'type': str,
-                'value': 'config_group'
-            },
-        }
-
-
-    @property
-    def parameterized_model_fields(self):
-        
-        return {
-        'parent': {
-            'blank': True,
-            'default': models.fields.NOT_PROVIDED,
-            'field_type': models.ForeignKey,
-            'null': True,
-            'unique': False,
-        },
-        'name': {
-            'blank': False,
-            'default': models.fields.NOT_PROVIDED,
-            'field_type': models.CharField,
-            'max_length': 50,
-            'null': False,
-            'unique': False,
-        },
-        'config': {
-            'blank': True,
-            'default': models.fields.NOT_PROVIDED,
-            'field_type': models.JSONField,
-            'null': True,
-            'unique': False,
-        },
-        'hosts': {
-            'blank': True,
-            'default': models.fields.NOT_PROVIDED,
-            'field_type': models.ManyToManyField,
-            'null': False,
-            'unique': False,
-        },
-        'modified': {
-            'blank': False,
-            'default': models.fields.NOT_PROVIDED,
-            'field_type': models.DateTimeField,
-            'null': False,
-            'unique': False,
-        },
-    }
 
 
 
