ff1085df24c092481fb86e9932635b1cc0eb5fe5
Jon <jonathon.lockwood@networkedweb.com>
2025-07-31 11:54:50 +0930

    refactor(itam): API Field Render Functional Test Suite re-written to PyTest for model Device
    ref: #920 #915

---
+++ app/itam/tests/functional/device/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+
+@pytest.fixture( scope = 'class')
+def model(model_device):
+
+    yield model_device
+
+
+@pytest.fixture( scope = 'class', autouse = True)
+def model_kwargs(request, kwargs_device):
+
+    request.cls.kwargs_create_item = kwargs_device.copy()
+
+    yield kwargs_device.copy()
+
+    if hasattr(request.cls, 'kwargs_create_item'):
+        del request.cls.kwargs_create_item
+
+
+@pytest.fixture( scope = 'class')
+def model_serializer(serializer_device):
+
+    yield serializer_device
--- app/tests/fixtures/model_device.py
+++ app/tests/fixtures/model_device.py
@@ -1,5 +1,6 @@
 import datetime
 import pytest
+import random
 
 from itam.models.device import Device
 from itam.serializers.device import (
@@ -17,9 +18,18 @@ def model_device():
 
 
 @pytest.fixture( scope = 'class')
-def kwargs_device(kwargs_centurionmodel):
+def kwargs_device(django_db_blocker, kwargs_centurionmodel,
+    model_devicemodel, kwargs_devicemodel,
+    model_devicetype, kwargs_devicetype,
+):
 
     random_str = str(datetime.datetime.now(tz=datetime.timezone.utc))
+
+    with django_db_blocker.unblock():
+
+        device_model = model_devicemodel.objects.create( **kwargs_devicemodel )
+
+        device_type = model_devicetype.objects.create( **kwargs_devicetype )
 
     kwargs = {
         **kwargs_centurionmodel.copy(),
@@ -27,12 +37,19 @@ def kwargs_device(kwargs_centurionmodel):
             ' ', '').replace(':', '').replace('+', '').replace('.', ''),
         'serial_number': str(random_str).replace(
             ' ', '').replace(':', '').replace('+', '').replace('.', ''),
-        # 'uuid': '7318f7cc-e3e8-4680-a3bf-29d77ce44b78',
-        # 'device_model': '',
-        # 'device_type': '',
+        'uuid': '7318f7cc-e3e8-4680-a3bf-29d77ce' + str( random.randint(10000, 99999) ),
+        'device_model': device_model,
+        'device_type': device_type,
+        'config':  { 'a_dev_config_key': 'a_dev_config_value'},
+        'inventorydate': '2025-07-31T11:51:00Z',
     }
 
     yield kwargs.copy()
+
+    with django_db_blocker.unblock():
+
+        device_model.delete()
+        device_type.delete()
 
 
 @pytest.fixture( scope = 'class')
