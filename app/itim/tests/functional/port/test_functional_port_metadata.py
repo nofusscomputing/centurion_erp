6f82a054cd46a3471cb4fd9f1847be07daf97784
Jon <jonathon.lockwood@networkedweb.com>
2025-08-03 12:40:02 +0930

    refactor(itim): API Metadata Functional Test Suite re-written to Pytest for model Port
    ref: #932 #930

--- app/itim/tests/functional/port/test_functional_port_metadata.py
+++ app/itim/tests/functional/port/test_functional_port_metadata.py
@@ -8,8 +8,6 @@ from access.models.tenant import Tenant as Organization
 from access.models.team import Team
 from access.models.team_user import TeamUsers
 
-from api.tests.abstract.api_permissions_viewset import APIPermissions
-from api.tests.abstract.api_serializer_viewset import SerializersTestCases
 from api.tests.abstract.test_metadata_functional import MetadataAttributesFunctional
 
 from itim.models.services import Port
@@ -18,6 +16,7 @@ from settings.models.app_settings import AppSettings
 
 
 
+@pytest.mark.model_port
 class ViewSetBase:
 
     model = Port
@@ -218,18 +217,7 @@ class ViewSetBase:
 
 
 
-class PortPermissionsAPI(ViewSetBase, APIPermissions, TestCase):
-
-    pass
-
-
-
-class PortViewSet(ViewSetBase, SerializersTestCases, TestCase):
-
-    pass
-
-
-
+@pytest.mark.module_itim
 class PortMetadata(
     ViewSetBase,
     MetadataAttributesFunctional,
