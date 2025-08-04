6dc42d4cdbedf4e318e1f535fbcb54612fc2a179
Jon <jonathon.lockwood@networkedweb.com>
2025-08-04 15:45:39 +0930

    refactor(project_management): ViewSet Unit Test Suite re-written to Pytest for model ProjectState
    ref: #938 #936

--- app/project_management/tests/unit/project_state/conftest.py
+++ app/project_management/tests/unit/project_state/conftest.py
@@ -17,3 +17,9 @@ def model_kwargs(request, kwargs_projectstate):
 
     if hasattr(request.cls, 'kwargs_create_item'):
         del request.cls.kwargs_create_item
+
+
+@pytest.fixture( scope = 'class')
+def model_serializer(serializer_projectstate):
+
+    yield serializer_projectstate
