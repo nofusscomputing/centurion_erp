9c73885f1b7c08cabdfe0cf85e7d618a0fdbaeb7
Jon <jonathon.lockwood@networkedweb.com>
2025-08-05 14:38:23 +0930

    refactor(access): Model Functional Test Suite re-written to Pytest for model Tenant
    ref: #942 #899

---
+++ app/access/tests/functional/tenant/test_functional_tenant_category_model.py
@@ -0,0 +1,28 @@
+import pytest
+
+from core.tests.functional.centurion_abstract.test_functional_centurion_abstract_model import (
+    CenturionAbstractModelInheritedCases
+)
+
+
+
+@pytest.mark.model_tenant
+class TenantModelTestCases(
+    CenturionAbstractModelInheritedCases
+):
+    pass
+
+
+
+class TenantModelInheritedCases(
+    TenantModelTestCases,
+):
+    pass
+
+
+
+@pytest.mark.module_assistance
+class TenantModelPyTest(
+    TenantModelTestCases,
+):
+    pass
