ecdd4e964c08dc79dbd0e3cdc2ebe79e6f6408a0
Jon <jonathon.lockwood@networkedweb.com>
2025-08-23 16:09:30 +0930

    feat(base): Add Group model ViewSet
    ref: #966 #551

--- app/centurion/viewsets/base/index.py
+++ app/centurion/viewsets/base/index.py
@@ -25,6 +25,7 @@ class Index(IndexViewset):
         return Response(
             {
                 "content_type": reverse('v2:_api_v2_content_type-list', request=request),
+                "groups": reverse('v2:_api_group-list', request=request),
                 "permission": reverse('v2:_api_permission-list', request=request),
                 "user": reverse('v2:_api_user-list', request=request)
             }
