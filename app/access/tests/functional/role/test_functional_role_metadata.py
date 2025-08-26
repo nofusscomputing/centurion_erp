606cab0aea9ce075058cccb7f1faf57e16378040
Jon <jonathon.lockwood@networkedweb.com>
2025-08-26 16:07:24 +0930

    chore(access): Update MetaData Test suites setup for new perms methods
    ref: #970 #551

--- app/access/tests/functional/role/test_functional_role_metadata.py
+++ app/access/tests/functional/role/test_functional_role_metadata.py
@@ -1,18 +1,10 @@
-import django
 import pytest
 
-from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
 from access.models.role import Role
-from access.models.tenant import Tenant
-from access.models.team import Team
-from access.models.team_user import TeamUsers
 
 from api.tests.abstract.test_metadata_functional import MetadataAttributesFunctional
-
-from settings.models.app_settings import AppSettings
 
 
 
@@ -53,174 +45,28 @@ class ViewSetBase:
         4. create a user per team
         """
 
-        User = django.contrib.auth.get_user_model()
+        super().presetUpTestData()
 
-        organization = Tenant.objects.create(name='test_org')
-
-        self.organization = organization
-
-        self.different_organization = Tenant.objects.create(name='test_different_organization')
-
-        self.global_organization = Tenant.objects.create(name='test_global_organization')
-
-        app_settings = AppSettings.objects.get(
-            owner_organization = None
-        )
-
-        app_settings.global_organization = self.global_organization
-
-        app_settings.save()
-
-        kwargs = self.kwargs_create_item
-        kwargs['organization'] = organization
-        kwargs['model_notes'] = 'some notes'
-
-        self.item = self.model.objects.create(
-            **kwargs
-        )
+        self.kwargs_create_item['organization'] = self.organization
+        self.kwargs_create_item['model_notes'] = 'some notes'
 
 
-        kwargs = self.kwargs_create_item
-        kwargs['organization'] = self.different_organization
-        kwargs['model_notes'] = 'some more notes'
-
-        self.other_org_item = self.model.objects.create(
-            **kwargs
-        )
+        self.kwargs_create_item_diff_org['organization'] = self.different_organization
+        self.kwargs_create_item_diff_org['model_notes'] = 'some more notes'
 
 
-        kwargs = self.kwargs_create_item
-        kwargs['organization'] = self.global_organization
-        kwargs['model_notes'] = 'some more notes'
+        self.kwargs_create_item_global_org_org['organization'] = self.global_organization
+        self.kwargs_create_item_global_org_org['model_notes'] = 'some more notes'
 
         self.global_org_item = self.model.objects.create(
-            **kwargs
+            **self.kwargs_create_item_global_org_org
         )
-
-
-        # self.url_kwargs = {'organization_id': self.organization.id}
-
-        self.url_view_kwargs.update({ 'pk': self.item.id })
 
         if self.add_data is not None:
 
             self.add_data.update({'organization': self.organization.id})
 
-
-        view_permissions = Permission.objects.get(
-                codename = 'view_' + self.model._meta.model_name,
-                content_type = ContentType.objects.get(
-                    app_label = self.model._meta.app_label,
-                    model = self.model._meta.model_name,
-                )
-            )
-
-        view_team = Team.objects.create(
-            team_name = 'view_team',
-            organization = organization,
-        )
-
-        view_team.permissions.set([view_permissions])
-
-
-
-        add_permissions = Permission.objects.get(
-                codename = 'add_' + self.model._meta.model_name,
-                content_type = ContentType.objects.get(
-                    app_label = self.model._meta.app_label,
-                    model = self.model._meta.model_name,
-                )
-            )
-
-        add_team = Team.objects.create(
-            team_name = 'add_team',
-            organization = organization,
-        )
-
-        add_team.permissions.set([add_permissions])
-
-
-
-        change_permissions = Permission.objects.get(
-                codename = 'change_' + self.model._meta.model_name,
-                content_type = ContentType.objects.get(
-                    app_label = self.model._meta.app_label,
-                    model = self.model._meta.model_name,
-                )
-            )
-
-        change_team = Team.objects.create(
-            team_name = 'change_team',
-            organization = organization,
-        )
-
-        change_team.permissions.set([change_permissions])
-
-
-
-        delete_permissions = Permission.objects.get(
-                codename = 'delete_' + self.model._meta.model_name,
-                content_type = ContentType.objects.get(
-                    app_label = self.model._meta.app_label,
-                    model = self.model._meta.model_name,
-                )
-            )
-
-        delete_team = Team.objects.create(
-            team_name = 'delete_team',
-            organization = organization,
-        )
-
-        delete_team.permissions.set([delete_permissions])
-
-
-        self.no_permissions_user = User.objects.create_user(username="test_no_permissions", password="password")
-
-
-        self.view_user = User.objects.create_user(username="test_user_view", password="password")
-        TeamUsers.objects.create(
-            team = view_team,
-            user = self.view_user
-        )
-
-        self.add_user = User.objects.create_user(username="test_user_add", password="password")
-        TeamUsers.objects.create(
-            team = add_team,
-            user = self.add_user
-        )
-
-        self.change_user = User.objects.create_user(username="test_user_change", password="password")
-        TeamUsers.objects.create(
-            team = change_team,
-            user = self.change_user
-        )
-
-        self.delete_user = User.objects.create_user(username="test_user_delete", password="password")
-        TeamUsers.objects.create(
-            team = delete_team,
-            user = self.delete_user
-        )
-
-
-        self.different_organization_user = User.objects.create_user(username="test_different_organization_user", password="password")
-
-
-        different_organization_team = Team.objects.create(
-            team_name = 'different_organization_team',
-            organization = self.different_organization,
-        )
-
-        different_organization_team.permissions.set([
-            view_permissions,
-            add_permissions,
-            change_permissions,
-            delete_permissions,
-        ])
-
-        TeamUsers.objects.create(
-            team = different_organization_team,
-            user = self.different_organization_user
-        )
+        super().setUpTestData()
 
 
 
