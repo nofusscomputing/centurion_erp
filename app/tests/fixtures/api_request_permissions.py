c51e97d8f3de4ecfbc2f2d446e998f38616137d5
Jon <jonathon.lockwood@networkedweb.com>
2025-08-31 16:24:39 +0930

    chore: ensure user is remove as part of fixture cleanup for api perms fixture
    ref: #978

--- app/tests/fixtures/api_request_permissions.py
+++ app/tests/fixtures/api_request_permissions.py
@@ -216,3 +216,5 @@ def api_request_permissions( django_db_blocker,
         different_organization_role.delete()
         different_organization_group.delete()
         different_organization_user.delete()
+
+        no_permission_user.delete()
