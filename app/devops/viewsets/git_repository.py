1d552d40d80904db5b46e73d64e559c0d66570c3
Jon <jonathon.lockwood@networkedweb.com>
2025-09-03 23:06:50 +0930

    chore(devops): update gitreposity vieset queryset -> _queryset
    ref: #982

--- app/devops/viewsets/git_repository.py
+++ app/devops/viewsets/git_repository.py
@@ -229,7 +229,7 @@ class ViewSet(
 
         if 'pk' in self.kwargs:
 
-            return self.queryset[0].get_url( request = self.request )
+            return self._queryset[0].get_url( request = self.request )
 
         return None
 
