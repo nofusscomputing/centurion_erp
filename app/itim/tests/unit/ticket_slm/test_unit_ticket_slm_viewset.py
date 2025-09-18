f02b72661e6873d1142ba2f0dcc25cd34129f6c6
Jon <jonathon.lockwood@networkedweb.com>
2025-09-18 16:48:47 +0930

    test(itim): Add ViewSet unit test suit for model SLMTicket
    ref: #1001 #727 #890

---
+++ app/itim/tests/unit/ticket_slm/test_unit_ticket_slm_viewset.py
@@ -0,0 +1,58 @@
+import pytest
+
+from types import NoneType
+
+from core.tests.unit.ticket_base.test_unit_ticket_base_viewset import (
+    TicketBaseViewsetInheritedCases
+)
+from core.viewsets.ticket import (
+    TicketBase,
+    ViewSet,
+)
+
+from itim.models.slm_ticket_base import (
+    SLMTicket
+)
+
+
+
+@pytest.mark.model_slmticket
+class ViewsetTestCases(
+    TicketBaseViewsetInheritedCases,
+):
+
+
+    @pytest.fixture( scope = 'function' )
+    def viewset(self):
+        return ViewSet
+
+
+    @property
+    def parameterized_class_attributes(self):
+        return {
+            'base_model': {
+                'value': TicketBase,
+            },
+            'model': {
+                'value': SLMTicket
+            },
+            'model_suffix': {
+                'type': NoneType,
+            },
+        }
+
+
+
+class SLMTicketBaseViewsetInheritedCases(
+    ViewsetTestCases,
+):
+    pass
+
+
+
+@pytest.mark.module_itim
+class SLMTicketBaseViewsetPyTest(
+    ViewsetTestCases,
+):
+
+    pass
