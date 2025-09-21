be122f3dce74b180a2ce2823ae86d5e5812ea92b
Jon <jonathon.lockwood@networkedweb.com>
2025-09-21 22:03:40 +0930

    chore(core): update TicketCommentSolution functional test suite api fields render to not check for parent field
    field not part of comment
    
    ref: #1008  #728

--- app/core/tests/functional/ticket_comment_solution/test_functional_ticket_solution_base_api_fields.py
+++ app/core/tests/functional/ticket_comment_solution/test_functional_ticket_solution_base_api_fields.py
@@ -1,5 +1,9 @@
 import pytest
+import random
 
+from django.db import models
+
+from core.models.ticket_base import TicketBase
 from core.tests.functional.ticket_comment_base.test_functional_ticket_comment_base_api_fields import (
     TicketCommentBaseAPIFieldsInheritedCases
 )
@@ -11,7 +15,104 @@ class TicketCommentSolutionAPITestCases(
     TicketCommentBaseAPIFieldsInheritedCases,
 ):
 
-    pass
+
+    @property
+    def parameterized_api_fields(self):
+
+        return {
+
+            'parent': {
+                'expected': type(None)
+            },
+            'parent.id': {
+                'expected': models.NOT_PROVIDED
+            },
+            'parent.display_name': {
+                'expected': models.NOT_PROVIDED
+            },
+            'parent.url': {
+                'expected': models.NOT_PROVIDED
+            },
+
+            '_urls.threads': {
+                'expected': models.NOT_PROVIDED
+            },
+        }
+
+
+    @pytest.fixture( scope = 'class')
+    def create_model(self, request, django_db_blocker,
+        model, model_kwargs
+    ):
+
+        with django_db_blocker.unblock():
+
+
+            kwargs = model_kwargs.copy()
+
+            kwargs['body'] = 'the template comment'
+
+            del kwargs['external_ref']
+            del kwargs['external_system']
+            del kwargs['category']
+
+            kwargs['comment_type'] = model._meta.sub_model_type
+            kwargs['is_template'] = True
+
+            template_comment = model.objects.create(
+                **kwargs
+            )
+
+
+            kwargs = model_kwargs.copy()
+            kwargs['template'] = template_comment
+
+            kwargs['ticket'].is_closed = False
+            kwargs['ticket'].date_closed = None
+            kwargs['ticket'].is_solved = False
+            kwargs['ticket'].date_solved = None
+            kwargs['ticket'].status = TicketBase.TicketStatus.NEW
+            kwargs['ticket'].save()
+
+            kwargs['external_ref'] = random.randint(333, 33333)
+
+            item = model.objects.create(
+                **kwargs
+            )
+
+            request.cls.item = item
+
+
+            kwargs = model_kwargs.copy()
+            kwargs['body'] = 'the child comment'
+            kwargs['comment_type'] = model._meta.sub_model_type
+            # kwargs['parent'] = request.cls.item
+
+            del kwargs['external_ref']
+            del kwargs['external_system']
+            del kwargs['category']
+
+            kwargs['ticket'].is_closed = False
+            kwargs['ticket'].date_closed = None
+            kwargs['ticket'].is_solved = False
+            kwargs['ticket'].date_solved = None
+            kwargs['ticket'].status = TicketBase.TicketStatus.NEW
+            kwargs['ticket'].save()
+
+
+            item_two = model.objects.create(
+                **kwargs
+            )
+
+            request.cls.item_two = item_two
+
+            yield item
+
+            item_two.delete()
+
+            item.delete()
+
+            template_comment.delete()
 
 
 
