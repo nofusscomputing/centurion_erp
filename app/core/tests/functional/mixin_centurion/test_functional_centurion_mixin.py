500d3d5b16e130252ebe5ffb2fbf337018bbd7e1
Jon <jonathon.lockwood@networkedweb.com>
2025-10-12 17:04:18 +0930

    test(core): Remove centrion mixin function test to check returning related model name
    test not required as this function is designed to use from base up, not model down whenfetching related name
    
    ref: #1027 #1026

--- app/core/tests/functional/mixin_centurion/test_functional_centurion_mixin.py
+++ app/core/tests/functional/mixin_centurion/test_functional_centurion_mixin.py
@@ -11,28 +11,6 @@ class CenturionAbstractMixinTestCases(
 ):
 
 
-    def test_method_get_related_field_name_returns_parent_model_name(self,
-        model, created_model
-    ):
-        """Test Class Method
-
-        Test to ensure that when function `get_related_field_name` is called
-        and the model is not the same as `._base_model`, it returns the name
-        of the parent model as a string
-        """
-
-        if model._meta.abstract:
-            pytest.xfail( reason = 'Model is abstract, test is N/A.' )
-
-        if not model()._base_model or model()._base_model is model:
-            pytest.xfail( reason = 'Not a sub-model, test is N/A.' )
-
-        base_object = getattr(created_model, f'{model()._base_model._meta.model_name}_ptr')
-
-        assert base_object.get_related_field_name() == created_model._meta.all_parents[0]._meta.model_name
-
-
-
     def test_method_get_related_model_returns_self(self,
         model, created_model
     ):
