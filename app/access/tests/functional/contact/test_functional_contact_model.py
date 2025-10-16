e2a6ef6c14329b6ca2cdb77bd7e580af89de2698
Jon <jonathon.lockwood@networkedweb.com>
2025-10-16 12:48:22 +0930

    test(access): Test cases to ensure that child model links to existing contact model
    ref: #1030 #1031

--- app/access/tests/functional/contact/test_functional_contact_model.py
+++ app/access/tests/functional/contact/test_functional_contact_model.py
@@ -11,11 +11,35 @@ class ContactModelTestCases(
     pass
 
 
-
 class ContactModelInheritedCases(
     ContactModelTestCases,
 ):
-    pass
+
+    def test_function_linked_model_contact__email(self,
+        model_contact,
+        model, model_kwargs,
+    ):
+        """Test linking to parent model `contact`
+
+        when fields `'email'` match, link to the model
+        """
+
+        if model._meta.pk.related_model is not model_contact:
+            pytest.xfail( reason = 'model not a child of contact. test is N/A.' )
+
+
+        kwargs = model_kwargs()
+
+        kwargs_contact = kwargs.copy()
+        del kwargs_contact['employee_number']
+        del kwargs_contact['user']
+
+        contact = model_contact.objects.create( **kwargs_contact )
+
+        obj = model.objects.create( **kwargs )
+
+        assert contact.id == obj.id
+
 
 
 
