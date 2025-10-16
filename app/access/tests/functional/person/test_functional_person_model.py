3172d17ea7a14d520e89ff51ada622893785e285
Jon <jonathon.lockwood@networkedweb.com>
2025-10-16 12:48:58 +0930

    test(access): Test cases to ensure that child model links to existing person model
    ref: #1030 closes #1031

--- app/access/tests/functional/person/test_functional_person_model.py
+++ app/access/tests/functional/person/test_functional_person_model.py
@@ -15,7 +15,121 @@ class PersonModelTestCases(
 class PersonModelInheritedCases(
     PersonModelTestCases,
 ):
-    pass
+
+    def test_function_linked_model_person__f_name_m_name_l_name_dob(self,
+        model_person,
+        model, model_kwargs,
+    ):
+        """Test linking to parent model `person`
+
+        when fields `'f_name', 'm_name', 'l_name', 'dob'` match, link to the model
+        """
+
+        if model._meta.pk.related_model is not model_person:
+            pytest.xfail( reason = 'model not a child of person. test is N/A.' )
+
+
+        kwargs = model_kwargs()
+
+        kwargs_person = kwargs.copy()
+        del kwargs_person['email']
+        del kwargs_person['directory']
+        kwargs_person['entity_type'] = 'person'
+
+        person = model_person.objects.create( **kwargs_person )
+
+        obj = model.objects.create( **kwargs )
+
+        assert person.id == obj.id
+
+
+    def test_function_linked_model_person__f_name_l_name_dob(self,
+        model_person,
+        model, model_kwargs,
+    ):
+        """Test linking to parent model `person`
+
+        when fields `'f_name', 'l_name', 'dob'` match, link to the model
+        """
+
+
+        if model._meta.pk.related_model is not model_person:
+            pytest.xfail( reason = 'model not a child of person. test is N/A.' )
+
+
+        kwargs = model_kwargs()
+
+        kwargs_person = kwargs.copy()
+        del kwargs_person['email']
+        del kwargs_person['directory']
+        del kwargs_person['m_name']
+        kwargs_person['entity_type'] = 'person'
+
+        person = model_person.objects.create( **kwargs_person )
+
+        obj = model.objects.create( **kwargs )
+
+        assert person.id == obj.id
+
+
+    def test_function_linked_model_person__f_name_m_name_l_name(self,
+        model_person,
+        model, model_kwargs,
+    ):
+        """Test linking to parent model `person`
+
+        when fields `'f_name', 'm_name', 'l_name'` match, link to the model
+        """
+
+
+        if model._meta.pk.related_model is not model_person:
+            pytest.xfail( reason = 'model not a child of person. test is N/A.' )
+
+
+        kwargs = model_kwargs()
+
+        kwargs_person = kwargs.copy()
+        del kwargs_person['email']
+        del kwargs_person['directory']
+        del kwargs_person['dob']
+        kwargs_person['entity_type'] = 'person'
+
+        person = model_person.objects.create( **kwargs_person )
+
+        obj = model.objects.create( **kwargs )
+
+        assert person.id == obj.id
+
+
+    def test_function_linked_model_person__f_name_l_name(self,
+        model_person,
+        model, model_kwargs,
+    ):
+        """Test linking to parent model `person`
+
+        when fields `'f_name', 'l_name'` match, link to the model
+        """
+
+
+        if model._meta.pk.related_model is not model_person:
+            pytest.xfail( reason = 'model not a child of person. test is N/A.' )
+
+
+        kwargs = model_kwargs()
+
+        kwargs_person = kwargs.copy()
+        del kwargs_person['email']
+        del kwargs_person['directory']
+        del kwargs_person['dob']
+        del kwargs_person['m_name']
+        kwargs_person['entity_type'] = 'person'
+
+        person = model_person.objects.create( **kwargs_person )
+
+        obj = model.objects.create( **kwargs )
+
+        assert person.id == obj.id
+
 
 
 
