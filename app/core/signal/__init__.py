cbb3a9df8b13aa9f4c58cb676fe25a243faa11a1
Jon <jonathon.lockwood@networkedweb.com>
2025-11-15 18:21:22 +0930

    feat(core): Add the ticket action comment signal
    ref: #1063 #1062 closes #503 #504

--- app/core/signal/__init__.py
+++ app/core/signal/__init__.py
@@ -1,2 +1,3 @@
 from .centurion_model_migrate import centurion_model_migrate    # pylint: disable=W0611:unused-import
 from .migra_manufacturer_to_company import manufacturer_to_company    # pylint: disable=W0611:unused-import
+from .ticket_action_comment import ticket_action_comment    # pylint: disable=W0611:unused-import
