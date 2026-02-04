4f960935556f8b81f3270cd4af6df5bf91dde590
Jon <jonathon.lockwood@networkedweb.com>
2026-02-04 09:43:52 +0930

    feat(base): Add oauth token required claim check to sso pipeline
    ref: #1108 #1097

--- app/centurion/settings.py
+++ app/centurion/settings.py
@@ -625,7 +625,9 @@ if SSO_ENABLED:
     SOCIAL_AUTH_PIPELINE = (
         'social_core.pipeline.social_auth.social_details',
         'social_core.pipeline.social_auth.social_uid',
+        'social_core.pipeline.social_auth.auth_allowed',    # added
         'social_core.pipeline.social_auth.social_user',
+        'centurion.auth.oauth.required_claim',
         'social_core.pipeline.user.get_username',
         'social_core.pipeline.social_auth.associate_by_email',
         'social_core.pipeline.user.create_user',
