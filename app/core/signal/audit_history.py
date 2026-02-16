3fb072b3295c9a3a3701e3742771eb1755a7071a
Jon <jonathon.lockwood@networkedweb.com>
2026-02-16 12:23:11 +0930

    feat(core): Capture Many2Many Field changes
    ref: #1122 #1064

--- app/core/signal/audit_history.py
+++ app/core/signal/audit_history.py
@@ -2,18 +2,26 @@ from django.apps import apps
 from django.conf import settings
 from django.contrib.auth.models import ContentType
 from django.db.models.signals import (
+    m2m_changed,
     post_save
 )
 from django.dispatch import receiver
 
 
 
+@receiver(m2m_changed, dispatch_uid="audit_history_save")
 @receiver(post_save, dispatch_uid="audit_history_save")
 def audit_history(sender, instance, **kwargs):
 
     if getattr(instance, '_audit_enabled', False):
 
-        if type(instance).context.get(instance._meta.model_name, None) is None:
+        if(
+            type(instance).context.get(instance._meta.model_name, None) is None
+            or (
+                kwargs['signal'] is m2m_changed
+                and kwargs['action'] not in [ 'post_add', 'post_remove' ]
+            )
+        ):
             return
         else:
             trace_var_for_testing = instance.context.get('user', None)
@@ -37,16 +45,30 @@ def audit_history(sender, instance, **kwargs):
 
 
         try:
-            audit_model.objects.create(
-                organization = instance.get_tenant(),
-                content_type = ContentType.objects.get(
-                    app_label = instance._meta.app_label,
-                    model = instance._meta.model_name,
-                ),
-                action = audit_action,
-                user = type(instance).context.get(instance._meta.model_name, None),
-                model = instance,
-            )
+
+            if kwargs['signal'] is post_save:
+
+                audit_entry = audit_model.objects.create(
+                    organization = instance.get_tenant(),
+                    content_type = ContentType.objects.get(
+                        app_label = instance._meta.app_label,
+                        model = instance._meta.model_name,
+                    ),
+                    action = audit_action,
+                    user = type(instance).context.get(instance._meta.model_name, None),
+                    model = instance,
+                )
+
+                instance.audit_entry = audit_entry
+
+            elif kwargs['signal'] is m2m_changed:
+
+                audit_entry = instance.audit_entry
+
+                instance._after = instance.get_audit_values()
+
+                audit_entry.save()
+
 
         except Exception as e:
             log.error(
