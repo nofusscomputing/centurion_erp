80b7de2385d1a021ed6e17060d3774f1aa9ab7bb
Jon <jonathon.lockwood@networkedweb.com>
2026-02-19 17:17:09 +0930

    feat(project_management): Add URL route for project tickets
    ref: #1126 #1047

--- app/project_management/urls_api.py
+++ app/project_management/urls_api.py
@@ -1,5 +1,9 @@
 from centurion_feature_flag.urls.routers import DefaultRouter
 
+from core.viewsets import (
+    ticket
+)
+
 from project_management.viewsets import (
     index as project_management,
     project,
@@ -10,6 +14,7 @@ from project_management.viewsets import (
 
 
 # app_name = "project_management"
+
 
 
 router: DefaultRouter = DefaultRouter(trailing_slash=False)
@@ -32,6 +37,12 @@ router.register(
     prefix = '/project/(?P<project_id>[0-9]+)/project_task',
     viewset = project_task.ViewSet,
     basename = '_api_v2_ticket_project_task'
+)
+
+router.register(
+    prefix = f'/project/(?P<project_id>[0-9]+)/(?P<ticket_type>[projecttask|ticket]+)',
+    viewset = ticket.ViewSet,
+    feature_flag = '2025-00006', basename = '_api_project_ticket'
 )
 
 
