582a747561888a26a3e3f6c109e87a77d44a5407
Jon <jonathon.lockwood@networkedweb.com>
2026-05-31 14:25:43 +0930

    fix(build): Add env var `CENTURION_STATIC_ROOT` for setting the static root path
    ref: #1148 #1147

--- app/centurion/settings.py
+++ app/centurion/settings.py
@@ -385,7 +385,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.getenv('CENTURION_STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
 
 STATICFILES_DIRS = [
     BASE_DIR / "project-static",
