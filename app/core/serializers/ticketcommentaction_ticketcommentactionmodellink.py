4d26260575605e5a43a9b170fb57c98c0a9fbf82
Jon <jonathon.lockwood@networkedweb.com>
2026-06-12 17:28:48 +0930

    feat(core): Add serializer for model TicketACtionCommentModelLink
    ref: #1167 #1161

---
+++ app/core/serializers/ticketcommentaction_ticketcommentactionmodellink.py
@@ -0,0 +1,39 @@
+from drf_spectacular.utils import extend_schema_serializer
+
+from core import fields as centurion_field
+from core.models.ticket_comment_action_model_link import TicketCommentActionModelLink
+from core.serializers.ticketcommentbase_ticketcommentaction import (
+    BaseSerializer,
+    ModelSerializer as TicketCommentBaseModelSerializer,
+    ViewSerializer as TicketCommentBaseViewSerializer
+)
+
+
+
+@extend_schema_serializer(component_name = 'TicketCommentActionModelLinkModelSerializer')
+class ModelSerializer(
+    TicketCommentBaseModelSerializer,
+    BaseSerializer,
+):
+
+
+    display_name = centurion_field.MarkdownField( required = True )
+
+
+    class Meta(TicketCommentBaseModelSerializer.Meta):
+
+        model = TicketCommentActionModelLink
+
+        fields = TicketCommentBaseModelSerializer.Meta.fields + [
+            'display_name',
+        ]
+
+
+
+@extend_schema_serializer(component_name = 'TicketCommentActionModelLinkViewSerializer')
+class ViewSerializer(
+    TicketCommentBaseViewSerializer,
+    ModelSerializer,
+):
+
+    pass
