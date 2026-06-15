6be7ac0616745471c9ece35db401d3a79f45f26a
Jon <jonathon.lockwood@networkedweb.com>
2026-06-15 14:49:33 +0930

    test(Unit): Add missing test cases for field checks for model TicketCommentActionFieldEdit
    ref: #1169 #1159

--- app/core/tests/unit/ticket_comment_action_field_edit/test_unit_ticket_comment_action_field_edit_model.py
+++ app/core/tests/unit/ticket_comment_action_field_edit/test_unit_ticket_comment_action_field_edit_model.py
@@ -1,5 +1,7 @@
 import pytest
 
+from django.db import models
+
 from core.models.ticket_comment_action import TicketCommentAction
 
 from core.tests.unit.ticket_comment_action.test_unit_ticket_comment_action_model import (
@@ -27,7 +29,29 @@ class TicketCommentActionFieldEditModelTestCases(
     @property
     def parameterized_model_fields(self):
 
-        return {}
+        return {
+            "field_name": {
+                'blank': False,
+                'default': models.fields.NOT_PROVIDED,
+                'field_type': models.CharField,
+                'null': False,
+                'unique': False,
+            },
+            "previous_value": {
+                'blank': True,
+                'default': models.fields.NOT_PROVIDED,
+                'field_type': models.CharField,
+                'null': True,
+                'unique': False,
+            },
+            "new_value": {
+                'blank': True,
+                'default': models.fields.NOT_PROVIDED,
+                'field_type': models.CharField,
+                'null': True,
+                'unique': False,
+            },
+        }
 
 
 
