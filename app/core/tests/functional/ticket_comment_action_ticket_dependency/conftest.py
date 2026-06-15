3708c86cc2a7d476ef345c4144064d9b2d03cbee
Jon <jonathon.lockwood@networkedweb.com>
2026-06-15 14:47:13 +0930

    test(functional): APIFields Test Suite for model TicketCommentActionTicketDependency
    ref: #1169 #1165

---
+++ app/core/tests/functional/ticket_comment_action_ticket_dependency/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+
+@pytest.fixture( scope = 'class')
+def model(request, model_ticketcommentactionticketdependency):
+
+    request.cls.model = model_ticketcommentactionticketdependency
+
+    yield request.cls.model
+
+    del request.cls.model
+
+
+@pytest.fixture( scope = 'class', autouse = True)
+def model_kwargs(request, kwargs_ticketcommentactionticketdependency):
+
+    request.cls.kwargs_create_item = kwargs_ticketcommentactionticketdependency()
+
+    yield kwargs_ticketcommentactionticketdependency
