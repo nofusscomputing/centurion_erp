62794bdc40cca1033d5ba1af8a556ba4923b8193
Jon <jonathon.lockwood@networkedweb.com>
2026-06-20 13:26:23 +0930

    test(core): Functional Test Case, delete removes inverse dependency for model TicketDependency
    ref: #1173 #1165

--- app/core/tests/functional/ticket_dependency/test_functional_ticket_dependency_model.py
+++ app/core/tests/functional/ticket_dependency/test_functional_ticket_dependency_model.py
@@ -26,4 +26,38 @@ class TicketDependencyModelInheritedCases(
 class TicketDependencyModelPyTest(
     TicketDependencyModelTestCases,
 ):
-    pass
+
+
+
+    def test_model_delete_removes_inverse_dependency(self, model, created_model):
+        """Model Delete Check
+
+        When a ticket dependency is deleted, it must also remove the ticket
+        dependency in the opposite direction.
+        """
+
+        db_model = model.objects.get( id = created_model.id )
+
+        ticket = db_model.ticket
+
+        dependent_ticket = db_model.dependent_ticket
+
+
+        model.objects.create(
+            ticket = dependent_ticket,
+            how_related = db_model.how_related,
+            dependent_ticket = ticket,
+            organization = dependent_ticket.organization,
+            user = db_model.user
+        )
+
+        db_model.delete()
+
+        db_check = model.objects.filter(
+            ticket = dependent_ticket,
+            dependent_ticket = ticket,
+        )
+
+
+        assert len(db_check) == 0
+
