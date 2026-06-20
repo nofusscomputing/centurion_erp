61af51d8a9e7c86ec203de86cd65ec80c2aef03a
Jon <jonathon.lockwood@networkedweb.com>
2026-06-20 12:48:52 +0930

    test(core): Functional Test Cases cover API CRUD import permission for model TicketCommentActionFieldEdit
    ref: #1173 #1159

--- app/core/tests/functional/additional_ticketcommentactionfieldedit_permissions_api.py
+++ app/core/tests/functional/additional_ticketcommentactionfieldedit_permissions_api.py
@@ -7,7 +7,6 @@ from django.test import Client
 class AdditionalTestCases:
 
 
-
     @pytest.mark.xfail( reason = 'User requires import permission for add action comment' )
     def test_api_add_exception(self, mocker, model_instance,
         parameterized, param_key_exceptions, param_value,
@@ -60,18 +59,61 @@ class AdditionalTestCases:
 
 
 
-    @pytest.mark.skip( reason = 'ToDo: Write test case' )
     def test_api_add_exception_import_user(self, mocker, model_instance,
         parameterized, param_key_exceptions, param_value,
         param_exception, param_http_status,
-        api_request_permissions, model_kwargs, kwargs_api_create, model
+        api_request_permissions, model_kwargs, kwargs_api_create, model,
+        model_contenttype
     ):
         """ API action capture exception
 
         Ensure that during a `Create` operation that the exception is captured
         and that the correct http status code is returned
         """
-        pass
+
+        user_role = api_request_permissions['user']['add'].groups.all(
+        )[0].roles.all()[0]
+
+        import_permission = model_contenttype.objects.get_for_model(
+            model
+        ).permission_set.get(
+            codename = f"import_{model._meta.model_name}"
+        )
+
+        user_role.permissions.add(import_permission)
+
+
+        client = Client()
+
+        client.force_login( api_request_permissions['user']['add'] )
+
+        kwargs = model_kwargs()
+        kwargs.update({
+            'organization': api_request_permissions['tenancy']['user']
+        })
+
+        the_model = model_instance( kwargs_create = kwargs )
+
+        url = the_model.get_url( many = True )
+
+        kwargs_create = kwargs_api_create.copy()
+        kwargs_create['created_by'] = api_request_permissions['user']['add'].id
+        kwargs_create['organization'] = api_request_permissions['tenancy']['user'].id
+
+        mocker.patch(
+            "access.managers.tenancy.TenancyManager.create",
+            side_effect = param_exception("an integrity error occured....")
+        )
+
+
+        response = client.post(
+            path = url,
+            data = kwargs_create,
+            content_type = 'application/json'
+        )
+
+
+        assert response.status_code == param_http_status, response.content
 
 
 
@@ -108,12 +150,50 @@ class AdditionalTestCases:
         assert response.status_code == 201, response.content\
 
 
-    @pytest.mark.skip( reason = 'ToDo: Write test case' )
+
     def test_permission_add_import_user(self, model_instance, api_request_permissions,
-        model_kwargs, kwargs_api_create,
+        model_kwargs, kwargs_api_create, model, model_contenttype
     ):
         """ Check correct permission for add 
 
         Attempt to add as user with "import" permission
         """
-        pass
+
+        user_role = api_request_permissions['user']['add'].groups.all(
+        )[0].roles.all()[0]
+
+        import_permission = model_contenttype.objects.get_for_model(
+            model
+        ).permission_set.get(
+            codename = f"import_{model._meta.model_name}"
+        )
+
+        user_role.permissions.add(import_permission)
+
+        client = Client()
+
+        client.force_login( api_request_permissions['user']['add'] )
+
+
+        kwargs = model_kwargs()
+        kwargs.update({
+            'organization': api_request_permissions['tenancy']['user']
+        })
+
+        the_model = model_instance( kwargs_create = kwargs )
+
+        url = the_model.get_url( many = True )
+
+        kwargs_create = kwargs_api_create.copy()
+        kwargs_create['created_by'] = api_request_permissions['user']['add'].id
+        kwargs_create['organization'] = api_request_permissions['tenancy']['user'].id
+
+
+        response = client.post(
+            path = url,
+            data = kwargs_create,
+            content_type = 'application/json'
+        )
+
+        assert response.status_code == 201, response.content\
+
