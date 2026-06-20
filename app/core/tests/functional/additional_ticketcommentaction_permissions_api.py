6af3e7f2042f7f83813d870134ca726dd1e50f4f
Jon <jonathon.lockwood@networkedweb.com>
2026-06-20 12:18:23 +0930

    feat(core): Limit ability to user whom has `import` permission for mode TicketCommentAction
    ref: #1173 #1159 #1161 #1165

--- app/core/tests/functional/additional_ticketcommentaction_permissions_api.py
+++ app/core/tests/functional/additional_ticketcommentaction_permissions_api.py
@@ -1,27 +1,84 @@
 import pytest
-import random
 
 from django.test import Client
-from django.urls.exceptions import NoReverseMatch
-
-from rest_framework.permissions import (
-    IsAuthenticatedOrReadOnly
-)
-
-from centurion_feature_flag.lib.feature_flag import CenturionFeatureFlagging
 
 
 
 class AdditionalTestCases:
 
 
+
+    @pytest.mark.xfail( reason = 'User requires import permission for add action comment' )
+    def test_api_add_exception(self, mocker, model_instance,
+        parameterized, param_key_exceptions, param_value,
+        param_exception, param_http_status,
+        api_request_permissions, model_kwargs, kwargs_api_create, model
+    ):
+
+        try:
+
+            client = Client()
+
+            client.force_login( api_request_permissions['user']['add'] )
+
+
+            kwargs = model_kwargs()
+            kwargs.update({
+                'organization': api_request_permissions['tenancy']['user']
+            })
+
+            the_model = model_instance( kwargs_create = kwargs )
+
+            url = the_model.get_url( many = True )
+
+            kwargs_create = kwargs_api_create.copy()
+            kwargs_create['created_by'] = api_request_permissions['user']['add'].id
+            kwargs_create['organization'] = api_request_permissions['tenancy']['user'].id
+
+            mocker.patch(
+                "access.managers.tenancy.TenancyManager.create",
+                side_effect = param_exception("an integrity error occured....")
+            )
+
+
+            response = client.post(
+                path = url,
+                data = kwargs_create,
+                content_type = 'application/json'
+            )
+
+
+            assert response.status_code == param_http_status, response.content
+
+        except AssertionError as ex:
+            pytest.xfail(
+                reason = (
+                    'User requires import permission for add action comment, '
+                    f'failure=[{ex}]'
+                )
+            )
+
+
+
+    @pytest.mark.skip( reason = 'ToDo: Write test case' )
+    def test_api_add_exception_import_user(self, mocker, model_instance,
+        parameterized, param_key_exceptions, param_value,
+        param_exception, param_http_status,
+        api_request_permissions, model_kwargs, kwargs_api_create, model
+    ):
+        """ API action capture exception
+
+        Ensure that during a `Create` operation that the exception is captured
+        and that the correct http status code is returned
+        """
+        pass
+
+
+
+    @pytest.mark.xfail( reason = 'Only import user can add an action comment' )
     def test_permission_add(self, model_instance, api_request_permissions,
         model_kwargs, kwargs_api_create,
     ):
-        """ Check correct permission for add 
-
-        Attempt to add as user with permission
-        """
 
         client = Client()
 
@@ -37,71 +94,26 @@ class AdditionalTestCases:
 
         url = the_model.get_url( many = True )
 
-        # the_model.delete()
-
         kwargs_create = kwargs_api_create.copy()
-        # kwargs_create['model'] = the_model.model.id
         kwargs_create['created_by'] = api_request_permissions['user']['add'].id
         kwargs_create['organization'] = api_request_permissions['tenancy']['user'].id
 
 
-        try:
-
-            response = client.post(
-                path = url,
-                data = kwargs_create,
-                content_type = 'application/json'
-            )
-
-        except NoReverseMatch:
-
-            # Cater for models that use viewset `-list` but `-detail`
-            try:
-
-                response = client.post(
-                    path = the_model.get_url( many = False ),
-                    data = kwargs_create
-                )
-
-            except NoReverseMatch:
-
-                pass
-
-
-        if response.status_code == 405:
-            pytest.xfail( reason = 'ViewSet does not have this request method.' )
-
-        assert response.status_code == 201, response.content
-
-
-
-    def test_permission_change(self, model_instance, api_request_permissions, model_kwargs,
-    ):
-        """ Check correct permission for change
-
-        Make change with user who has change permission
-        """
-
-        client = Client()
-
-        client.force_login( api_request_permissions['user']['change'] )
-
-        kwargs = model_kwargs()
-        kwargs.update({
-            'organization': api_request_permissions['tenancy']['user']
-        })
-
-        change_item = model_instance(
-            kwargs_create = kwargs,
-        )
-
-        response = client.patch(
-            path = change_item.get_url( many = False ),
-            data = self.change_data,
+        response = client.post(
+            path = url,
+            data = kwargs_create,
             content_type = 'application/json'
         )
 
-        if response.status_code == 405:
-            pytest.xfail( reason = 'ViewSet does not have this request method.' )
+        assert response.status_code == 201, response.content\
 
-        assert response.status_code == 200, response.content
+
+    @pytest.mark.skip( reason = 'ToDo: Write test case' )
+    def test_permission_add_import_user(self, model_instance, api_request_permissions,
+        model_kwargs, kwargs_api_create,
+    ):
+        """ Check correct permission for add 
+
+        Attempt to add as user with "import" permission
+        """
+        pass
