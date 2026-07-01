1e960b1e8addbb0f687f2805802bd8517c67623c
Jon <jonathon.lockwood@networkedweb.com>
2026-07-01 15:40:59 +0930

    test(project_management): Model Functional test case to confirm field duration_project calculates correctly
    ref: #1188 #1187

--- app/project_management/tests/functional/project/test_functional_project_model.py
+++ app/project_management/tests/functional/project/test_functional_project_model.py
@@ -10,7 +10,68 @@ from core.tests.functional.centurion_abstract.test_functional_centurion_abstract
 class ProjectModelTestCases(
     CenturionAbstractTenancyModelInheritedCases
 ):
-    pass
+
+    duration_params = [
+        # name, duration_slash_command, expected_seconds
+        ('single_ticket_single_comment', [ '1h' ], 3600),
+        ('single_ticket_multiple_comment', [ '30m', '30m' ], 3600),
+        ('multiple_ticket_single_comment', [[ '30m' ], [ '30m' ]], 3600),
+        ('multiple_ticket_multiple_comment', [[ '15m', '15m' ], [ '15m', '15m' ]], 3600),
+    ]
+
+    @pytest.mark.parametrize(
+        argnames = ['name', 'duration_slash_command', 'expected_seconds'],
+        argvalues = duration_params,
+        ids = [ name for name, *other in duration_params ],
+    )
+    def test_field_duration(self, created_model,
+        name, duration_slash_command, expected_seconds,
+        model_projecttaskticket, kwargs_projecttaskticket,
+        model_ticketcommentbase, kwargs_ticketcommentbase
+    ):
+        """Test field
+
+        Ensure that field `duration_project` calculates the correct time
+        """
+
+        ticket_comment_kwargs = kwargs_ticketcommentbase()
+
+        def task_factory():
+            kwargs = kwargs_projecttaskticket()
+            del kwargs['milestone']
+            kwargs['project'] = created_model
+
+            return model_projecttaskticket.objects.create( **kwargs )
+
+
+        if isinstance(duration_slash_command[0], str ):
+
+            task = task_factory()
+
+            for comment in duration_slash_command:
+
+                model_ticketcommentbase.objects.create(
+                    ticket = task,
+                    body = f"a comment\n/spend {comment}",
+                    user = ticket_comment_kwargs['user']
+                )
+
+        elif isinstance(duration_slash_command[0], list):
+
+            for ticket in duration_slash_command:
+
+                task = task_factory()
+
+                for comment in ticket:
+
+                    model_ticketcommentbase.objects.create(
+                        ticket = task,
+                        body = f"a comment\n/spend {comment}",
+                        user = ticket_comment_kwargs['user']
+                    )
+
+
+        assert created_model.duration_project == expected_seconds
 
 
 
