a8a1e83835711875911d34d5bce8eabcbd7a331d
Jon <jonathon.lockwood@networkedweb.com>
2026-07-01 15:47:32 +0930

    test(project_management): Skipped Model Functional test case to confirm field estimation_project calculates correctly.
    As slash command estimation does not exist, this test will be skipped although ready to use when implemented.
    
    ref: #1188 #273 closes #1187

--- app/project_management/tests/functional/project/test_functional_project_model.py
+++ app/project_management/tests/functional/project/test_functional_project_model.py
@@ -75,6 +75,64 @@ class ProjectModelTestCases(
 
 
 
+    @pytest.mark.skip( reason = 'No slash command for estimation yet.' )
+    @pytest.mark.parametrize(
+        argnames = ['name', 'duration_slash_command', 'expected_seconds'],
+        argvalues = duration_params,
+        ids = [ name for name, *other in duration_params ],
+    )
+    def test_field_estimation(self, created_model,
+        name, duration_slash_command, expected_seconds,
+        model_projecttaskticket, kwargs_projecttaskticket,
+        model_ticketcommentbase, kwargs_ticketcommentbase
+    ):
+        """Test field
+
+        Ensure that field `estimation_project` calculates the correct time
+        """
+
+        ticket_comment_kwargs = kwargs_ticketcommentbase()
+
+        def task_factory():
+            kwargs = kwargs_projecttaskticket()
+            del kwargs['milestone']
+            kwargs['project'] = created_model
+
+            return model_projecttaskticket.objects.create( **kwargs )
+
+
+        if isinstance(duration_slash_command[0], str ):
+
+            task = task_factory()
+
+            for comment in duration_slash_command:
+
+                model_ticketcommentbase.objects.create(
+                    ticket = task,
+                    body = f"a comment\n/estimate {comment}",
+                    user = ticket_comment_kwargs['user']
+                )
+
+        elif isinstance(duration_slash_command[0], list):
+
+            for ticket in duration_slash_command:
+
+                task = task_factory()
+
+                for comment in ticket:
+
+                    model_ticketcommentbase.objects.create(
+                        ticket = task,
+                        body = f"a comment\n/spend {comment}",
+                        user = ticket_comment_kwargs['user']
+                    )
+
+
+        assert created_model.estimation_project == 0
+
+
+
+
 class ProjectModelInheritedCases(
     ProjectModelTestCases,
 ):
