7da8bc023d3424e59622514910e01692e65b9833
Jon <jonathon.lockwood@networkedweb.com>
2026-08-01 14:21:32 +0930

    fix(core): default to filter by `model_id` for ModelTicket unless query is for tickets a model has
    ref: #1208 #1207

--- app/core/tests/unit/model_tickets/test_unit_model_tickets_viewset.py
+++ app/core/tests/unit/model_tickets/test_unit_model_tickets_viewset.py
@@ -23,7 +23,14 @@ class ViewsetTestCases(
 
     @pytest.fixture( scope = 'function' )
     def viewset(self):
-        return ViewSet
+
+        viewset = ViewSet
+
+        viewset.kwargs = {'model_id': 1}
+
+        yield ViewSet
+
+        del viewset
 
 
     @property
@@ -129,4 +136,205 @@ class ModelTicketViewsetInheritedCases(
 class ModelTicketViewsetPyTest(
     ViewsetTestCases,
 ):
-    pass
+
+
+    @pytest.mark.xfail( reason = 'Model requires kwarg model_id' )
+    def test_function_get_queryset_manager_calls_user(self, mocker, model, viewset):
+
+        manager = mocker.patch.object(model, 'objects' )
+
+        view_set = viewset()
+        view_set.request = mocker.Mock()
+        view_set.kwargs =  {}
+
+        if model._is_submodel:
+            view_set.kwargs =  {
+                view_set.model_kwarg: model._meta.model_name
+            }
+
+        view_set.get_queryset()
+
+        manager.user.assert_called()
+
+
+
+    def test_function_get_queryset_manager_calls_user_custom_kwarg_model_id(
+        self, mocker, model, viewset
+    ):
+        """Test class function
+
+        Ensure that when function `get_queryset` the manager is first called with
+        `.user()` so as to ensure that the queryset returns only data the user has
+        access to.
+        """
+
+        manager = mocker.patch.object(model, 'objects' )
+
+        view_set = viewset()
+        view_set.request = mocker.Mock()
+        view_set.kwargs =  {
+            'model_id': 1
+        }
+
+        view_set.get_queryset()
+
+        manager.user.assert_called()
+
+
+
+    @pytest.mark.xfail( reason = 'Model requires kwarg model_id' )
+    def test_function_get_queryset_manager_filters_by_pk(self, mocker, model, viewset):
+
+        manager = mocker.patch.object(model, 'objects' )
+
+        view_set = viewset()
+
+        view_set.request = mocker.Mock()
+
+        view_set.kwargs =  {
+            'pk': 1
+        }
+
+        view_set.get_queryset()
+
+        manager.user.return_value.all.return_value.filter.assert_called_once_with(pk=1)
+
+
+
+    def test_function_get_queryset_manager_filters_by_pk_custom_kwarg_model_id(
+        self, mocker, model, viewset
+    ):
+        """Test class function
+
+        Ensure that when function `get_queryset` the queryset is filtered by `pk` kwarg
+        """
+
+        manager = mocker.patch.object(model, 'objects' )
+
+        view_set = viewset()
+
+        view_set.request = mocker.Mock()
+
+        view_set.kwargs =  {
+            'pk': 1,
+            'model_id': 1
+        }
+
+        view_set.get_queryset()
+
+        manager.user.return_value.all.return_value.filter.assert_called_once_with(pk=1)
+
+
+
+    @pytest.mark.xfail( reason = 'Model requires kwarg model_id' )
+    def test_view_func_get_queryset_cache_result(self, mocker, viewset_mock_request):
+
+        view_set = viewset_mock_request
+        view_set.kwargs = {
+            'model_id': 1
+        }
+        mocker.patch.object(view_set.model, 'objects', return_value = 'boo')
+
+        from django.db import connection
+        from django.test.utils import CaptureQueriesContext
+
+        with CaptureQueriesContext(connection) as queries:
+
+            assert view_set._queryset is None    # Must be empty before init
+
+            view_set.get_queryset()
+
+            initial_db_queries = len(queries)
+
+            assert view_set._queryset is not None    # Must not be empty after init
+
+            assert len(queries) == initial_db_queries
+
+
+
+    def test_view_func_get_queryset_cache_result_custom_kwarg_model_id(
+        self, mocker, viewset_mock_request
+    ):
+        """Viewset Test
+
+        Ensure that the `get_queryset` function caches the result under
+        attribute `<viewset>._queryset`
+        """
+
+        view_set = viewset_mock_request
+        view_set.kwargs = {
+            'model_id': 1
+        }
+        mocker.patch.object(view_set.model, 'objects', return_value = 'boo')
+
+        from django.db import connection
+        from django.test.utils import CaptureQueriesContext
+
+        with CaptureQueriesContext(connection) as queries:
+
+            assert view_set._queryset is None    # Must be empty before init
+
+            view_set.get_queryset()
+
+            initial_db_queries = len(queries)
+
+            assert view_set._queryset is not None    # Must not be empty after init
+
+            assert len(queries) == initial_db_queries
+
+
+
+    @pytest.mark.xfail( reason = 'Model requires kwarg model_id' )
+    def test_view_func_get_queryset_cache_result_used(self, mocker, viewset, viewset_mock_request):
+
+        view_set = viewset_mock_request
+
+        qs = mocker.spy(view_set.model, 'objects')
+
+        view_set.get_queryset()    # Initial QuerySet fetch/filter and cache
+
+        initial_method_calls = len(qs.method_calls)
+        initial_mock_calls = len(qs.mock_calls)
+
+        # one call to .all()
+        assert initial_method_calls > 0
+        # calls = .user( ...), .user().all(), .user().all().filter()
+        assert initial_mock_calls > 0
+
+        view_set.get_queryset()    # Use Cached results, dont re-fetch QuerySet
+
+        assert len(qs.method_calls) == initial_method_calls
+        assert len(qs.mock_calls) == initial_mock_calls
+
+
+
+    def test_view_func_get_queryset_cache_result_used_custom_kwarg_model_id(
+        self, mocker, viewset, viewset_mock_request
+    ):
+        """Viewset Test
+
+        Ensure that the `get_queryset` function caches the result under
+        attribute `<viewset>._queryset`
+        """
+
+        view_set = viewset_mock_request
+        view_set.kwargs = {
+            'model_id': 1
+        }
+
+        qs = mocker.spy(view_set.model, 'objects')
+
+        view_set.get_queryset()    # Initial QuerySet fetch/filter and cache
+
+        initial_method_calls = len(qs.method_calls)
+        initial_mock_calls = len(qs.mock_calls)
+
+        # one call to .all()
+        assert initial_method_calls > 0
+        # calls = .user( ...), .user().all(), .user().all().filter()
+        assert initial_mock_calls > 0
+
+        view_set.get_queryset()    # Use Cached results, dont re-fetch QuerySet
+
+        assert len(qs.method_calls) == initial_method_calls
+        assert len(qs.mock_calls) == initial_mock_calls
