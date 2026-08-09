551482da302910dd3d2eb53760831317e668642c
Jon <jonathon.lockwood@networkedweb.com>
2026-08-09 17:22:58 +0930

    test(core): Re-add ViewSet Functional test cases that were skipped for TicketDependency
    unknown why they were skipped, so re-added.
    
    ref: #1224 #1207

--- app/core/tests/functional/ticket_dependency/test_functional_ticket_dependency_viewset.py
+++ app/core/tests/functional/ticket_dependency/test_functional_ticket_dependency_viewset.py
@@ -71,23 +71,6 @@ class ViewsetTestCases(
 
 
 
-    def test_function_get_meta_urls_self_url(self):
-        pytest.xfail( reason = 'Base class does not require test' )
-
-
-    def test_function_get_meta_urls_no_sub_models_key(self):
-        pytest.xfail( reason = 'Base class does not require test' )
-
-
-    def test_function_get_meta_urls_sub_models_keys(self):
-        pytest.xfail( reason = 'Base class does not require test' )
-
-
-    def test_function_get_meta_urls_sub_models_values(self,):
-        pytest.xfail( reason = 'Base class does not require test' )
-
-
-
 class TicketDependencyViewsetInheritedCases(
     ViewsetTestCases,
 ):
