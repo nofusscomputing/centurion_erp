486268c3e7b2873c92b95862b1a7e41368e0dc8f
Jon <jonathon.lockwood@networkedweb.com>
2026-09-14 12:38:55 +0930

    fix(core): When fecthing comments to calculate ticket duration, ensure threads are also gathered.
    ref: #1239 #1238

--- app/core/models/ticket_base.py
+++ app/core/models/ticket_base.py
@@ -240,7 +240,7 @@ class TicketBase(
     @property
     def ticket_duration(self) -> int:
 
-        comments = self.get_comments()
+        comments = self.get_comments( include_threads = True )
 
         duration = comments.aggregate(models.Sum('duration'))['duration__sum']
 
