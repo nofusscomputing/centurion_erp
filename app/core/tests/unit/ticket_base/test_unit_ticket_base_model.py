7d622312fd4892e2a9921dc18778556443e09a3f
Jon <jonathon.lockwood@networkedweb.com>
2026-09-14 13:25:48 +0930

    test(core): Unit test cases for ticketbase get comments confirming with and without threads
    ref: #1239 #1238

--- app/core/tests/unit/ticket_base/test_unit_ticket_base_model.py
+++ app/core/tests/unit/ticket_base/test_unit_ticket_base_model.py
@@ -789,7 +789,53 @@ class TicketBaseModelTestCases(
         assert type(model().get_comments()) is QuerySet
 
 
-    def test_function_called_clean_ticketbase(self, model, mocker, model_kwargs):
+
+    def test_function_param_get_comments_no_threads(self, model, model_ticketcommentbase, mocker):
+        """Function test, call params
+
+        Ensure that when function `get_comments` is called with default params
+        that threads are not included
+        """
+
+        my_model = model()
+
+        mocker.patch.object(my_model, 'id', 987621)
+
+        objects_call = mocker.patch.object(model_ticketcommentbase, 'objects')
+
+        my_model.get_comments()
+
+        print(objects_call.filter)
+
+        objects_call.filter.assert_called_with(
+            ticket = 987621,
+            parent = None,
+        )
+
+
+
+    def test_function_param_get_comments_threads(self, model, model_ticketcommentbase, mocker):
+        """Function test, call params
+
+        Ensure that when function `get_comments` is called with default params
+        that threads are not included
+        """
+
+        my_model = model()
+
+        mocker.patch.object(my_model, 'id', 91187621)
+
+        objects_call = mocker.patch.object(model_ticketcommentbase, 'objects')
+
+        my_model.get_comments( include_threads = True )
+
+        objects_call.filter.assert_called_with(
+            ticket = 91187621
+        )
+
+
+
+    def test_function_param_called_clean_ticketbase(self, model, mocker, model_kwargs):
         """Function Check
 
         Ensure function `TicketBase.clean` is called
