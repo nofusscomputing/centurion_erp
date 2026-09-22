961df339415cf73a4b741403cd84499c51c5fd88
Jon <jonathon.lockwood@networkedweb.com>
2026-09-22 14:52:52 +0930

    test(core): Functional test case to confirm double space slash commands in ticket description still function
    ref: #1247 #1241

--- app/core/tests/functional/slash_commands/test_slash_command_related.py
+++ app/core/tests/functional/slash_commands/test_slash_command_related.py
@@ -151,6 +151,46 @@ class SlashCommandsTicketTestCases(
 
 
 
+    def test_slash_command_ticket_single_line_command_own_line_lf_command_removed_from_description_dbl_space(self,
+        ticket,
+        parameterized, param_key_slash_command, param_name,
+        param_slash_command,
+        param_command_obj,
+    ):
+        """Slash command Check
+
+        Ensure the command is removed from a comment.
+
+        This test case specifically tests a command with double spaces as the
+        separator, i.e. `/<command>\s\s<object>`
+        """
+
+        comment_text = self.single_line_command_own_line_lf
+
+        assert 'COMMAND' in comment_text
+        # COMMAND must be in ticket comment so it can be constructed
+
+        command_obj = str(param_command_obj).replace(
+            'EXISTINGTICKET', str(self.existing_ticket.id)
+        )
+
+        ticket.description = str(
+            comment_text.replace(
+                'COMMAND', '/' + param_slash_command + '  ' + command_obj
+            )
+        )
+
+
+        ticket.save()
+
+
+        assert (
+            param_slash_command not in ticket.description
+            and command_obj not in ticket.description
+        )
+
+
+
 
     def test_slash_command_ticket_single_line_command_own_line_lf_command_removed_from_description(self,
         ticket,
